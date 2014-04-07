42acd312902f70b892be5e3d52080f8e57902cb4
Joel Akeret <jakeret@phys.ethz.ch>
2014-04-07 14:43:45 +0000

    major refactoring
    git-svn-id: svn+ssh://login.phys.ethz.ch/data/astro/refreg/repositories/PynPointRep@58 fad95807-5a3c-4d51-968a-885bb2ae6072

--- PynPoint/_cache.py
+++
@@ -1,7 +0,0 @@
-#to manage PynPoint variables that will be stored in the PynPoint context
-
-class im_arr_store:
-    def __init__(self,im_arr):
-        self.im_arr = im_arr
-    def get(self):
-        return self.im_arr
