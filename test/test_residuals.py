9f2e19e04be575b414a6e84c0061f8c07bda15d5
Joel Akeret <jakeret@phys.ethz.ch>
2014-04-22 08:45:23 +0000

    added gplv3 license
    git-svn-id: svn+ssh://login.phys.ethz.ch/data/astro/refreg/repositories/PynPointRep@99 fad95807-5a3c-4d51-968a-885bb2ae6072

--- test/test_residuals.py
+++ test/test_residuals.py
@@ -1,8 +1,20 @@
-
 # Copyright (C) 2014 ETH Zurich, Institute for Astronomy
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/.
+
 
 """
-Tests for `PynPoint_v1_5` module.
+Tests for `Residual` module.
 """
 # from __future__ import print_function, division, absolute_import, unicode_literals
 
@@ -17,7 +29,7 @@ limit1 = 1e-10
 limit2 = 2e-4
 
 
-class TestPynpoint_v1_5(object):
+class TestResidual(object):
 
     def setup(self):
         #prepare unit test. Load data etc
