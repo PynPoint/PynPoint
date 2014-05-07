fb6bc4a701aebccc27b7864ee43a3c0534bdfed1
Adam Amara <adam.amara@phys.ethz.ch>
2014-05-07 14:55:03 +0000

    removing commented out lines and print statements.
    git-svn-id: svn+ssh://login.phys.ethz.ch/data/astro/refreg/repositories/PynPointRep@116 fad95807-5a3c-4d51-968a-885bb2ae6072

--- test/test_Mask.py
+++ test/test_Mask.py
@@ -29,18 +29,9 @@ limit2 = 1.0e-7
 class TestMask(object):
 
     def setup(self):
-        #prepare unit test. Load data etc
-        print("setting up " + __name__)
-        #file1 = '/Users/amaraa/Work/Active_Projects/PynPoint_svn_sync/PynPoint/Tests/Test_data/Naco_M_im_temp.hdf5'
-        #file2 = '/Users/amaraa/Work/Active_Projects/PynPoint_svn_sync/PynPoint/Tests/Test_data/Naco_M_basis.hdf5'
-#         mask1 = PynPoint.mask(100,200,pattern='circle',para_ini=0.0,fsize=0.15,fxcent=0.2,fycent=0.5)
-#         mask2 = PynPoint.mask(200,200,pattern='circle',para_ini=20.0,fsize=0.15,fxcent=0.2,fycent=0.5)
-#         self.mask1 = mask1
-#         self.mask2 = mask2
-        #self.file2 = file2
-        #self.basis = basis
 
-        pass
+        print("setting up " + __name__)
+
 
     def test_mk_circle(self):
         im_temp = _Mask.mk_circle(400,400,100.,200.,20.)
@@ -52,42 +43,7 @@ class TestMask(object):
         assert im_temp[200,121] == 1.0
         assert im_temp[200,79] == 1.0
 
-#     def test_initialisation(self):
-#         assert self.mask1.xnum == 100
-#         assert self.mask1.ynum == 200
-#         assert self.mask1.para_ini == 0.0
-#         assert self.mask1.fsize == 0.15
-#         assert self.mask1.size_pix == 15.
-#         assert self.mask1.fxcent == 0.2
-#         assert self.mask1.xcent == 20.0
-#         assert self.mask1.fycent == 0.5
-#         assert self.mask1.ycent== 50.0
-#         assert self.mask1.pattern == 'circle'
-#         assert len(self.mask1.mask_base.shape) == 2
-#         assert self.mask1.mask_base.shape[0] == 100
-#         assert self.mask1.mask_base.shape[1] == 200
-#         assert self.mask1.mask_base.min() == 0.0
-#         assert self.mask1.mask_base.max() == 1.0
-#         assert self.mask1.mask_base[50,20] == 0.0
-#         assert self.mask1.mask_base[50,28] == 1.0
-#         assert self.mask1.mask_base[50,27] == 0.0        
 
-
-#     def test_mask(self):
-#         mask_rot1 = self.mask2.mask(del_para=90)
-#         assert self.mask2.mask_base[100,40] == 0.0
-#         assert mask_rot1.shape[0] == 200
-#         assert mask_rot1.shape[1] == 200
-#         assert mask_rot1.max() == 1.0
-#         assert mask_rot1.min() == 0.0
-#         assert mask_rot1[40,100] == 0.0
-#         assert self.mask2.temp_xcent == 100.
-#         assert self.mask2.temp_ycent == 40.
-
-#     def test_plt_mask(self):
-#         self.mask1.plt_mask(para=None)
-#         self.mask2.plt_mask(para=90)
-        
     def test_mk_cent_remove(self):
         temp_arr = np.ones([10,400,400])*np.pi
         im_arr_omask,im_arr_imask,cent_mask = _Mask.mk_cent_remove(temp_arr,cent_size=0.2,edge_size=1.0)
