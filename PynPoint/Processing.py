7bf51f60f98c2a0a7816e674f6d83329bb91c2a5
mbonse@uni-koblenz.de <38-Sonnen>
2016-06-13 16:44:22 +0200

    Ports and first .fits reading

--- PynPoint/Processing.py
+++ PynPoint/Processing.py
@@ -1,5 +1,10 @@
+# external modules
 from abc import ABCMeta, abstractmethod, abstractproperty
 import os
+import warnings
+
+# own modules
+from DataIO import OutputPort
 
 class PypelineModule:
     __metaclass__ = ABCMeta
@@ -14,11 +19,17 @@ class PypelineModule:
         return self._m_name
 
     @abstractmethod
+    def connect_database(self,
+                         data_base_in):
+        pass
+
+    @abstractmethod
     def run(self):
         pass
 
 
 class WritingModule(PypelineModule):
+    __metaclass__ = ABCMeta
 
     def __init__(self,
                  name_in=None,
@@ -32,11 +43,8 @@ class WritingModule(PypelineModule):
                                                                        '%s' % output_dir
         self.m_output_location = output_dir
 
-    # TODO remove this run function
-    def run(self):
-        print "Working hard"
-
     # TODO: Ports
+
 
 
 class ProcessingModule(PypelineModule):
@@ -48,7 +56,34 @@ class ProcessingModule(PypelineModule):
 
 
 class ReadingModule(PypelineModule):
+    __metaclass__ = ABCMeta
 
-    def __init__(self):
-        # TODO: Ports
-        pass
+    def __init__(self,
+                 name_in=None,
+                 input_dir=None):
+        super(ReadingModule, self).__init__(name_in)
+        # TODO Documentation
+
+        # If input_dir is None its location will be the Pypeline default input directory
+        assert (os.path.isdir(str(input_dir)) or input_dir is None), 'Error: Input directory for reading module' \
+                                                                       ' does not exist - input requested: ' \
+                                                                       '%s' % input_dir
+        self.m_input_location = input_dir
+        self._m_out_ports = {}
+
+    def add_output_port(self,
+                        tag,
+                        default_activation=True):
+        # TODO Documentation
+
+        tmp_port = OutputPort(tag,
+                              default_activation)
+        if tag in self._m_out_ports:
+            warnings.warn('Tag already used. Updating..')
+
+        self._m_out_ports[tag] = tmp_port
+
+    def connect_database(self,
+                         data_base_in):
+        for key, port in self._m_out_ports.iteritems():
+            port.set_database_connection(data_base_in)
