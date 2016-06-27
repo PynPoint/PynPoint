f542b46ad8eb1a5d924d1265869167aa5fdd6825
mbonse@uni-koblenz.de <38-Sonnen>
2016-06-27 19:57:33 +0200

    Moving non static header to Data Ports other stuff for Exporting as hdf5

---
+++ PynPoint/Hdf5Writing.py
@@ -0,0 +1,52 @@
+"""
+Module for Writing a list of tags from the database to a separated .hdf5 file
+"""
+
+import h5py
+
+from PynPoint.Processing import WritingModule
+
+
+class Hdf5WritingModule(WritingModule):
+
+    def __init__(self,
+                 name_in,
+                 file_name,
+                 output_dir=None,
+                 tag_dictionary=None,
+                 keep_attributes = True):
+
+        super(Hdf5WritingModule, self).__init__(name_in, output_dir)
+        self.m_file_name = file_name
+        self.m_tag_dictionary = tag_dictionary
+        self.m_keep_attributes = keep_attributes
+
+        # Add Ports
+        for key in tag_dictionary:
+            self.add_input_port(key)
+
+    def run(self):
+
+        # create new .hdf5 file
+        out_file = h5py.File((self.m_output_location + '/'+ self.m_file_name), mode='a')
+
+        for in_tag, out_tag in self.m_tag_dictionary.iteritems():
+
+            tmp_data = self._m_input_ports[in_tag].get_all()
+
+            data_set = out_file.create_dataset(out_tag,
+                                               data=tmp_data)
+
+            if self.m_keep_attributes:
+                # stable attributes
+                tmp_attr = self._m_input_ports[in_tag].get_all_attributes()
+
+                # it is not possible to copy attributes all together
+                for key, value in tmp_attr.iteritems():
+                    data_set.attrs[key] = value
+
+                # non stable attributes
+                # check if there is a header group
+
+        out_file.close()
+
--- PynPoint/PSFsubPreparation.py
+++ PynPoint/PSFsubPreparation.py
@@ -1,3 +1,10 @@
+"""
+Module for data Preparation before PSF Subtraction. Includes:
+    - Resizing
+    - Masking
+    - Image normalization
+"""
+
 from __future__ import division
 
 import numpy as np
@@ -18,7 +25,6 @@ class PSFdataPreparation(ProcessingModule):
                  resize=False,
                  cent_remove=True,
                  F_final=2.0,
-                 ran_sub=None,
                  para_sort=True,
                  cent_size=0.05,
                  edge_size=1.0):
@@ -28,8 +34,7 @@ class PSFdataPreparation(ProcessingModule):
         # Note recentering is not longer supported
         self.m_resize = resize
         self.m_cent_remove = cent_remove
-        self.m_F_final = F_final
-        self.m_ran_sub = ran_sub
+        self.m_f_final = F_final
         self.m_para_sort = para_sort
         self.m_cent_size = cent_size
         self.m_edge_size = edge_size
@@ -47,63 +52,52 @@ class PSFdataPreparation(ProcessingModule):
         self.add_output_port(image_out_tag)
         self.add_output_port("header_" + image_in_tag + "/im_norm")
 
-    def run(self):
+    @staticmethod
+    def _im_norm(im_data_in):
 
-        # TODO para_sort
+        im_norm = (im_data_in.sum(axis=1)).sum(axis=1)
 
-        # image normalization
-        im_data = self._m_input_ports[self.m_image_in_tag].get_all()
+        for i in range(0, len(im_data_in[:, 0, 0])):
+            im_data_in[i, ] /= im_norm[i]
 
-        print im_data[0,0,0]
+        return im_norm
 
-        im_norm = (im_data.sum(axis = 1)).sum(axis = 1)
-        print(im_norm)
+    def _im_resizing(self,
+                     im_data_in):
+        x_num_final, y_num_final = int(im_data_in.shape[1] * self.m_f_final),\
+                                      int(im_data_in.shape[2] * self.m_f_final)
+        im_arr_res = np.zeros([im_data_in.shape[0], x_num_final, y_num_final])
 
-        for i in range(0,len(im_data[:, 0, 0])):
-            im_data[i, ] /= im_norm[i]
+        for i in range(0, im_data_in.shape[0]):
+            im_tmp = im_data_in[i]
+            im_tmp = ndimage.interpolation.zoom(im_tmp,
+                                                [self.m_f_final,
+                                                 self.m_f_final],
+                                                order=5)
+            im_arr_res[i,] = im_tmp
 
-        # TODO del this line ?
-        self._m_output_ports[self.m_norm_port].set_all(im_norm)
-        self._m_output_ports[self.m_image_out_tag][:] = im_data
+        return im_arr_res
 
-        # image resizing
-        if self.m_resize:
-            xnum_final,  ynum_final = int(im_data.shape[1] * self.m_F_final),\
-                                      int(im_data.shape[2] * self.m_F_final)
-            im_arr_res = np.zeros([im_data.shape[0], xnum_final, ynum_final])
+    def _im_masking(self,
+                    im_data_in):
 
-            for i in range(0, im_data.shape[0]):
-                im_tmp = im_data[i]
-                im_tmp = ndimage.interpolation.zoom(im_tmp,
-                                                    [self.m_F_final,
-                                                     self.m_F_final],
-                                                    order=3)  # > 5 TODO
-                im_arr_res[i,] = im_tmp
-
-            self._m_output_ports[self.m_image_out_tag].set_all(im_arr_res,
-                                                               keep_attributes = True)
-
-        # image masking
-
-        def mk_circle_func(center_x,center_y):
+        def mk_circle_func(center_x, center_y):
             """sets up a function for calculating the radius to x,y (after having been initialised
             with x_cent and y_cent) """
-            return lambda x,y:np.sqrt((center_x-x)**2 +(center_y-y)**2)
+            return lambda x, y: np.sqrt((center_x-x)**2 +(center_y-y)**2)
 
-        def mk_circle(xnum,ynum,xcent,ycent,rad_lim):
+        def mk_circle(x_num, y_num, x_cent, y_cent, rad_lim):
             """function for making a circular aperture"""
-            Y,X = np.indices([xnum,ynum]) #seems strange and backwards, check!
-            rad = mk_circle_func(xcent,ycent)(X,Y)
-            i,j = np.where(rad <= rad_lim)
-            mask_base = np.ones((xnum,ynum),float) #something strange about the order of x and y!
-            mask_base[i,j] = 0.0
+            y_val, x_val = np.indices([x_num, y_num])
+            rad = mk_circle_func(x_cent, y_cent)(x_val, y_val)
+            i, j = np.where(rad <= rad_lim)
+            mask_base = np.ones((x_num, y_num), float)
+            mask_base[i, j] = 0.0
             return mask_base
 
-        im_data = self._m_input_ports[self.m_image_in_tag].get_all()
-        im_size = im_data[0, ].shape
+        im_size = im_data_in[0, ].shape
 
         if self.m_cent_remove:
-
 
             mask_c = mk_circle(im_size[0],
                                im_size[1],
@@ -119,18 +113,61 @@ class PSFdataPreparation(ProcessingModule):
 
             cent_mask = mask_c * (1.0 - mask_outside)
             res_cent_mask = (1.0 - cent_mask)
-            im_arr_imask = im_data * res_cent_mask
-            self._m_output_ports[self.m_image_mask_out_tag].set_all(im_arr_imask)
+            im_arr_i_mask = im_data_in * res_cent_mask
+            self._m_output_ports[self.m_image_mask_out_tag].set_all(im_arr_i_mask)
 
-            im_arr_omask = im_data * cent_mask
-            self._m_output_ports[self.m_image_out_tag].set_all(im_arr_omask,
-                                                               keep_attributes=True)
+            im_arr_o_mask = im_data_in * cent_mask
 
+            self._m_output_ports[self.m_mask_out_tag].set_all(cent_mask)
+            return im_arr_o_mask
 
         else:
             cent_mask = np.ones(im_size)
 
         self._m_output_ports[self.m_mask_out_tag].set_all(cent_mask)
+        return im_data_in
+
+    def run(self):
+
+        im_data = self._m_input_ports[self.m_image_in_tag].get_all()
+
+        # image normalization
+        im_norm = self._im_norm(im_data)
+        self._m_output_ports[self.m_norm_port].set_all(im_norm)
+
+        # TODO para_sort
+
+        # image resizing
+        if self.m_resize:
+            im_data = self._im_resizing(im_data)
+
+        # image masking
+        im_data = self._im_masking(im_data)
+
+        self._m_output_ports[self.m_image_out_tag].set_all(im_data,
+                                                           keep_attributes=True)
+
+        # save attributes
+        self._m_output_ports[self.m_image_out_tag].add_attribute("cent_remove",
+                                                                 self.m_cent_remove)
+
+        self._m_output_ports[self.m_image_out_tag].add_attribute("resize",
+                                                                 self.m_resize)
+
+        self._m_output_ports[self.m_image_out_tag].add_attribute("para_sort",
+                                                                 self.m_para_sort)
+
+        self._m_output_ports[self.m_image_out_tag].add_attribute("F_final",
+                                                                 str(self.m_f_final))
+
+        self._m_output_ports[self.m_image_out_tag].add_attribute("cent_size",
+                                                                 str(self.m_cent_size))
+
+        self._m_output_ports[self.m_image_out_tag].add_attribute("edge_size",
+                                                                 str(self.m_edge_size))
+
+
+
 
 
 
