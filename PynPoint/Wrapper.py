70be1e70007aa0389445c4e08d1b0689a8b0bbca
mbonse@uni-koblenz.de <38-Sonnen>
2016-06-27 19:57:33 +0200

    Moving non static header to Data Ports other stuff for Exporting as hdf5

--- PynPoint/Wrapper.py
+++ PynPoint/Wrapper.py
@@ -1,25 +1,28 @@
 import numpy as np
 import warnings
+import os
 
 # own modules
 from PynPoint.Pypeline import Pypeline
 from FitsReading import ReadFitsCubesDirectory
 from PSFsubPreparation import PSFdataPreparation
+from Hdf5Writing import Hdf5WritingModule
 
 class BasePynpointWrapper(object):
 
     def __init__(self):
         pass
 
-    def save(self,filename):
+    def save(self, filename):
         pass
+
 
     def plt_im(self,ind):
         pass
 
 
 
-class ImageWrapper(BasePynpointWrapper):
+class ImageWrapper(object):
 
     class_counter = 1
 
@@ -34,9 +37,11 @@ class ImageWrapper(BasePynpointWrapper):
         # In the old PynPoint it was possible to create multiple image instances working on
         # separated data (in memory). Hence, every time a new ImageWrapper is created a new database
         # entry is required
-        self._m_image_data_tag = "im_arr" + str(ImageWrapper.class_counter)
-        self._m_image_data_masked_tag = "im_mask_arr" + str(ImageWrapper.class_counter)
-        self._m_mask_tag = "mask_arr" + str(ImageWrapper.class_counter)
+        self._m_image_data_tag = "im_arr" + str(ImageWrapper.class_counter).zfill(2)
+        self._m_image_data_masked_tag = "im_mask_arr" + str(ImageWrapper.class_counter).zfill(2)
+        self._m_mask_tag = "mask_arr" + str(ImageWrapper.class_counter).zfill(2)
+        #self._m_counter = str(ImageWrapper.class_counter).zfill(2)
+        # TODO figure out if this is needed
         ImageWrapper.class_counter += 1
 
     def __getattr__(self, item):
@@ -49,9 +54,6 @@ class ImageWrapper(BasePynpointWrapper):
         elif item == "im_size":
             return (self._pypeline.get_data(self._m_image_data_tag).shape[1],
                     self._pypeline.get_data(self._m_image_data_tag).shape[2])
-
-        elif item == "cent_remove":
-            return self._m_center_remove
 
         elif item == "im_arr":
             return self._pypeline.get_data(self._m_image_data_tag)
@@ -68,6 +70,24 @@ class ImageWrapper(BasePynpointWrapper):
         elif item == "im_arr_mask":
             return self._pypeline.get_data(self._m_image_data_masked_tag)
 
+        # Attributes
+        elif item == "cent_remove":
+            return bool(self._pypeline.get_attribute(self._m_image_data_tag, "cent_remove"))
+
+        elif item == "resize":
+            return bool(self._pypeline.get_attribute(self._m_image_data_tag, "resize"))
+
+        elif item == "para_sort":
+            return bool(self._pypeline.get_attribute(self._m_image_data_tag, "para_sort"))
+
+        elif item == "F_final":
+            return float(self._pypeline.get_attribute(self._m_image_data_tag, "F_final"))
+
+        elif item == "cent_size":
+            return float(self._pypeline.get_attribute(self._m_image_data_tag, "cent_size"))
+
+        elif item == "edge_size":
+            return float(self._pypeline.get_attribute(self._m_image_data_tag, "edge_size"))
 
     @classmethod
     def create_wdir(cls, dir_in, **kwargs):
@@ -106,8 +126,7 @@ class ImageWrapper(BasePynpointWrapper):
                                          image_out_tag=obj._m_image_data_tag,
                                          resize=obj._m_resize,
                                          cent_remove=obj._m_center_remove,
-                                         cent_size=obj._m_cent_size,
-                                         ran_sub=obj._m_ran_sub)
+                                         cent_size=obj._m_cent_size)
 
         # TODO check all parameters
 
@@ -115,6 +134,33 @@ class ImageWrapper(BasePynpointWrapper):
         obj._pypeline.add_module(preparation)
         obj._pypeline.run()
         return obj
+
+    def save(self, filename):
+
+        filename = str(filename)
+
+        if os.path.isfile(filename):
+            warnings.warn('The file %s have been overwritten' % filename)
+
+        head, tail = os.path.split(filename)
+
+        dictionary = {self._m_image_data_tag: "im_arr",
+                     self._m_image_data_masked_tag: "im_mask_arr",
+                     self._m_mask_tag: "mask_arr"}
+
+        writing = Hdf5WritingModule("hdf5_writing",
+                                    tail,
+                                    output_dir=head,
+                                    tag_dictionary=dictionary,
+                                    keep_attributes=True)
+
+        self._pypeline.add_module(writing)
+
+        self._pypeline.run_module("hdf5_writing")
+
+
+    def plt_im(self,ind):
+        pass
 
     @classmethod
     def create_whdf5input(cls, file_in,**kwargs):
