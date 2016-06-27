9a060ad783dffdcb4347727493b532dcdaaf8c2f
majobodo <m.bonse@gmx.de>
2016-06-27 19:57:33 +0200

    Moving non static header to Data Ports other stuff for Exporting as hdf5

--- PynPoint/FitsReading.py
+++ PynPoint/FitsReading.py
@@ -80,12 +80,15 @@ class ReadFitsCubesDirectory(ReadingModule):
                              True)
         self._m_overwrite = force_overwrite_in_databank
 
+        # get location of the Reading module
+        script_dir = os.path.dirname(__file__)
+
         # read NACO static and non static keys
-        static_keys_file = open("PynPoint/config/NACO_static_header_keys.txt", "r")
+        static_keys_file = open(script_dir + "/config/NACO_static_header_keys.txt", "r")
         self.m_static_keys = static_keys_file.read().split(",\n")
         static_keys_file.close()
 
-        non_static_keys_file = open("PynPoint/config/NACO_non_static_header_keys.txt", "r")
+        non_static_keys_file = open(script_dir + "/config/NACO_non_static_header_keys.txt", "r")
         self.m_non_static_keys = non_static_keys_file.read().split(",\n")
         non_static_keys_file.close()
 
@@ -108,13 +111,6 @@ class ReadFitsCubesDirectory(ReadingModule):
             non_static_keys_file = open(kwargs['new_non_static'], "r")
             self.m_non_static_keys.extend(non_static_keys_file.read().split(",\n"))
             non_static_keys_file.close()
-
-        # create port for each non-static key
-        for key in self.m_non_static_keys:
-            self.add_output_port("/header_"+self.m_image_tag+"/"+key)
-
-        # create port for file list
-        self.add_output_port("/header_"+self.m_image_tag+"/"+"Used_Files")
 
     def _read_single_file(self,
                           fits_file,
@@ -163,8 +159,8 @@ class ReadFitsCubesDirectory(ReadingModule):
         # Use the NACO / VLT specific header tags
         for key in tmp_header:
             if key in self.m_static_keys:
-                check = self._m_out_ports[self.m_image_tag].check_attribute(key,
-                                                                            tmp_header[key])
+                check = self._m_out_ports[self.m_image_tag].check_static_attribute(key,
+                                                                                   tmp_header[key])
                 if check == -1:
                     warnings.warn('Static keyword %s has changed. Probably the current '
                                   'file %s does not belong to the data set "%s" of the PynPoint'
@@ -175,34 +171,36 @@ class ReadFitsCubesDirectory(ReadingModule):
                     pass
                 else:
                     # Attribute is new -> add
-                    self._m_out_ports[self.m_image_tag].add_attribute(key, tmp_header[key])
+                    self._m_out_ports[self.m_image_tag].add_attribute(key,
+                                                                      tmp_header[key],
+                                                                      static=True)
 
             elif key in self.m_non_static_keys:
-                # use Port
-                port_key = "/header_"+self.m_image_tag+"/"+key
-                if self._m_overwrite and key not in overwrite_keys:
-                    # delete key
-                    self._m_out_ports[port_key].set_all(np.asarray([tmp_header[key],]))
-                    overwrite_keys.append(key)
-                else:
-                    self._m_out_ports[port_key].append(np.asarray([tmp_header[key],]))
+                self._m_out_ports[self.m_image_tag].append_attribute_data(key,
+                                                                          tmp_header[key])
+
             elif key is not "":
                 warnings.warn('Unknown Header "%s" key found' %str(key))
 
-            hdulist.close()
+        hdulist.close()
+
+        # append used file to header information
+        self._m_out_ports[self.m_image_tag].append_attribute_data("Used_Files",
+                                                                  tmp_location + fits_file)
 
     def run(self):
         """
         Run method of the module. It looks for all .fits files in the input directory and reads them
         using the internal function _read_single_file().
         Note if self._m_overwrite = force_overwrite_in_databank is True old data base information is
-        overwritten by the module.
+        overwritten by the module. Furthermore the module saves the number of files and the actual
+        files used to create the database entry as attributes.
 
         :return: None
         """
 
         if not self.m_input_location.endswith('/'):
-            tmp_location = self.m_input_location +'/'
+            tmp_location = self.m_input_location + '/'
         else:
             tmp_location = self.m_input_location
 
@@ -226,26 +224,13 @@ class ReadFitsCubesDirectory(ReadingModule):
             self._m_out_ports[self.m_image_tag].flush()
         self._m_out_ports[self.m_image_tag].close_port()
 
-        # TODO add to function Documentation
-        # add number of files and files used to create the database entry as attribute / header
+        # Update number of files used to create the data base entry
         new_files_num = len(files)
-        new_files = []
-        for fits_file in files:
-            new_files.append(tmp_location+ fits_file)
-
         if self._m_overwrite:
             # no old files have been used
             self._m_out_ports[self.m_image_tag].open_port()
             self._m_out_ports[self.m_image_tag].add_attribute("Num_Files", new_files_num)
-
-            self._m_out_ports["/header_"+self.m_image_tag+"/"+"Used_Files"]\
-                .set_all(np.asarray(new_files))
-
-            self._m_out_ports["/header_"+self.m_image_tag+"/"+"Used_Files"].close_port()
-            self._m_out_ports[self.m_image_tag].close_port()
         else:
             # appended data
-            self._m_out_ports[self.m_image_tag].open_port()
             new_files_num += self._m_out_ports[self.m_image_tag].get_attribute("Num_Files")
             self._m_out_ports[self.m_image_tag].add_attribute("Num_Files", new_files_num)
-            self._m_out_ports[self.m_image_tag].close_port()
