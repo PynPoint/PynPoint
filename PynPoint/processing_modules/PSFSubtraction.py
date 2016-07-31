74af4dc52c2dd5d9c4697bbc344163fdf7da0125
majobodo <mbonse@uni-koblenz.de>
2016-07-31 19:11:57 +0200

    Finished End to End Pipeline

--- PynPoint/processing_modules/PSFSubtraction.py
+++ PynPoint/processing_modules/PSFSubtraction.py
@@ -2,6 +2,168 @@ import numpy as np
 from scipy import linalg, ndimage
 
 from PynPoint.core.Processing import ProcessingModule
+from PSFsubPreparation import PSFdataPreparation
+
+
+class PSFSubtractionModule(ProcessingModule):
+
+    def __init__(self,
+                 pca_number,
+                 name_in="PSF_subtraction",
+                 images_in_tag="im_arr",
+                 reference_in_tag="ref_arr",
+                 res_arr_out_tag="res_arr",
+                 res_arr_rot_out_tag="res_rot",
+                 res_mean_tag="res_mean",
+                 res_median_tag="res_median",
+                 res_var_tag="res_var",
+                 res_rot_mean_clip_tag="res_rot_mean_clip",
+                 extra_rot=0.0,
+                 **kwargs):
+
+        super(PSFSubtractionModule, self).__init__(name_in)
+
+        # additional keywords
+        if "basis_tag" in kwargs:
+            basis_tag = kwargs["basis_out_tag"]
+        else:
+            basis_tag = "pca_basis_set"
+
+        if "image_ave_tag" in kwargs:
+            im_average_tag = kwargs["image_ave_tag"]
+        else:
+            im_average_tag = "image_average"
+
+        if "psf_model_tag" in kwargs:
+            psf_model_tag = kwargs["psf_model_tag"]
+        else:
+            psf_model_tag = "psf_model"
+
+        if "cent_mask_tag" in kwargs:
+            cent_mask_tag = kwargs["cent_mask_tag"]
+        else:
+            cent_mask_tag = "cent_mask_tag"
+
+        if "cent_remove" in kwargs:
+            cent_remove = kwargs["cent_remove"]
+        else:
+            cent_remove = False
+
+        if "cent_size" in kwargs:
+            cent_size = kwargs["cent_size"]
+            cent_remove = True
+        else:
+            cent_size = 0.05
+
+        if "edge_size" in kwargs:
+            edge_size = kwargs["edge_size"]
+            cent_remove = True
+        else:
+            edge_size = 1.0
+
+        if "ref_prep_tag" in kwargs:
+            ref_prep_tag = kwargs["ref_prep_tag"]
+        else:
+            ref_prep_tag = "ref_prep"
+
+        if "prep_tag" in kwargs:
+            prep_tag = kwargs["prep_tag"]
+        else:
+            prep_tag = "data_prep"
+
+        self.m_num_components = pca_number
+
+        self._m_preparation_images = PSFdataPreparation(name_in="prep_im",
+                                                        image_in_tag=images_in_tag,
+                                                        image_out_tag=prep_tag,
+                                                        image_mask_out_tag="not_needed",
+                                                        mask_out_tag=cent_mask_tag,
+                                                        cent_remove=cent_remove,
+                                                        para_sort=False,
+                                                        cent_size=cent_size,
+                                                        edge_size=edge_size)
+
+        self._m_preparation_images.m_image_mask_out_port.deactivate()
+
+        self._m_preparation_reference = PSFdataPreparation(name_in="prep_ref",
+                                                           image_in_tag=reference_in_tag,
+                                                           image_out_tag=ref_prep_tag,
+                                                           image_mask_out_tag="not_needed",
+                                                           mask_out_tag=cent_mask_tag,
+                                                           cent_remove=cent_remove,
+                                                           para_sort=False,
+                                                           cent_size=cent_size,
+                                                           edge_size=edge_size)
+
+        self._m_preparation_reference.m_image_mask_out_port.deactivate()
+
+        self._m_make_pca_basis = MakePCABasisModule(im_arr_in_tag=ref_prep_tag,
+                                                    im_arr_out_tag="not_needed",
+                                                    im_average_out_tag=im_average_tag,
+                                                    basis_out_tag=basis_tag)
+
+        self._m_make_pca_basis.m_im_arr_out_port.deactivate()
+
+        self._m_make_psf_model = MakePSFModelModule(num=pca_number,
+                                                    im_arr_in_tag=prep_tag,
+                                                    basis_in_tag=basis_tag,
+                                                    basis_average_in_tag=im_average_tag,
+                                                    psf_basis_out_tag=psf_model_tag)
+
+        self._m_residuals_module = \
+            CreateResidualsModule(im_arr_in_tag=prep_tag,
+                                  psf_im_in_tag=psf_model_tag,
+                                  mask_in_tag=cent_mask_tag,
+                                  res_arr_out_tag=res_arr_out_tag,
+                                  res_arr_rot_out_tag=res_arr_rot_out_tag,
+                                  res_mean_tag=res_mean_tag,
+                                  res_median_tag=res_median_tag,
+                                  res_var_tag=res_var_tag,
+                                  res_rot_mean_clip_tag=res_rot_mean_clip_tag,
+                                  extra_rot=extra_rot)
+
+    def connect_database(self,
+                         data_base_in):
+
+        self._m_preparation_images.connect_database(data_base_in)
+        self._m_preparation_reference.connect_database(data_base_in)
+        self._m_make_pca_basis.connect_database(data_base_in)
+        self._m_make_psf_model.connect_database(data_base_in)
+        self._m_residuals_module.connect_database(data_base_in)
+
+        super(PSFSubtractionModule, self).connect_database(data_base_in)
+
+    def run(self):
+
+        print "Preparing data..."
+        self._m_preparation_images.run()
+        self._m_preparation_reference.run()
+        print "Finished preparing data..."
+        print "Creating PCA-basis set..."
+        self._m_make_pca_basis.run()
+        print "Finished creating PCA-basis set..."
+        print "Calculating PSF-Model..."
+        self._m_make_psf_model.run()
+        print "Finished calculating PSF-model..."
+        print "Creating residuals..."
+        self._m_residuals_module.run()
+        print "Finished creating residuals..."
+
+        # Take Header Information
+        input_port = self._m_residuals_module.m_im_arr_in_port
+
+        out_ports = [self._m_residuals_module.m_res_arr_out_port,
+                     self._m_residuals_module.m_res_arr_rot_out_port,
+                     self._m_residuals_module.m_res_mean_port,
+                     self._m_residuals_module.m_res_median_port,
+                     self._m_residuals_module.m_res_var_port,
+                     self._m_residuals_module.m_res_rot_mean_clip_port]
+
+        history = "PCA with " + str(self.m_num_components) + " PCA-components"
+        for port in out_ports:
+            port.copy_attributes_from_input_port(input_port)
+            port.add_history_information("PSF_subtraction",
+                                         history)
 
 
 class CreateResidualsModule(ProcessingModule):
@@ -22,24 +184,33 @@ class CreateResidualsModule(ProcessingModule):
         super(CreateResidualsModule, self).__init__(name_in)
 
         # Inputs
-        self._m_im_arr_in_port = self.add_input_port(im_arr_in_tag)
+        if mask_in_tag is None:
+            self._m_mask_in_port = None
+        else:
+            self._m_mask_in_port = self.add_input_port(mask_in_tag)
+
+        self.m_im_arr_in_port = self.add_input_port(im_arr_in_tag)
         self._m_psf_im_port = self.add_input_port(psf_im_in_tag)
-        self._m_mask_in_port = self.add_input_port(mask_in_tag)
 
         # Outputs
-        self._m_res_arr_out_port = self.add_output_port(res_arr_out_tag)
-        self._m_res_arr_rot_out_port = self.add_output_port(res_arr_rot_out_tag)
-        self._m_res_mean_port = self.add_output_port(res_mean_tag)
-        self._m_res_median_port = self.add_output_port(res_median_tag)
-        self._m_res_var_port = self.add_output_port(res_var_tag)
-        self._m_res_rot_mean_clip_port = self.add_output_port(res_rot_mean_clip_tag)
+        self.m_res_arr_out_port = self.add_output_port(res_arr_out_tag)
+        self.m_res_arr_rot_out_port = self.add_output_port(res_arr_rot_out_tag)
+        self.m_res_mean_port = self.add_output_port(res_mean_tag)
+        self.m_res_median_port = self.add_output_port(res_median_tag)
+        self.m_res_var_port = self.add_output_port(res_var_tag)
+        self.m_res_rot_mean_clip_port = self.add_output_port(res_rot_mean_clip_tag)
 
         self.m_extra_rot = extra_rot
 
     def run(self):
-        im_data = self._m_im_arr_in_port.get_all()
+        im_data = self.m_im_arr_in_port.get_all()
         psf_im = self._m_psf_im_port.get_all()
-        cent_mask = self._m_mask_in_port.get_all()
+
+        if self._m_mask_in_port is None:
+            cent_mask = np.zeros((im_data.shape[1],
+                                 im_data.shape[2]))
+        else:
+            cent_mask = self._m_mask_in_port.get_all()
 
         # create result array
         res_arr = im_data.copy()
@@ -48,7 +219,7 @@ class CreateResidualsModule(ProcessingModule):
             res_arr[i, ] -= (psf_im[i, ] * cent_mask)
 
         # rotate result array
-        para_angles = self._m_im_arr_in_port.get_attribute("NEW_PARA")
+        para_angles = self.m_im_arr_in_port.get_attribute("NEW_PARA")
         delta_para = para_angles[0] - para_angles
         res_rot = np.zeros(shape=im_data.shape)
         for i in range(0, len(delta_para)):
@@ -88,12 +259,12 @@ class CreateResidualsModule(ProcessingModule):
                     res_rot_mean_clip[i, j] = temp.mean() + b2.mean()
 
         # save results
-        self._m_res_arr_out_port.set_all(res_arr)
-        self._m_res_arr_rot_out_port.set_all(res_rot)
-        self._m_res_mean_port.set_all(tmp_res_rot_mean)
-        self._m_res_median_port.set_all(tmp_res_rot_median)
-        self._m_res_var_port.set_all(tmp_res_rot_var)
-        self._m_res_rot_mean_clip_port.set_all(res_rot_mean_clip)
+        self.m_res_arr_out_port.set_all(res_arr)
+        self.m_res_arr_rot_out_port.set_all(res_rot)
+        self.m_res_mean_port.set_all(tmp_res_rot_mean)
+        self.m_res_median_port.set_all(tmp_res_rot_median)
+        self.m_res_var_port.set_all(tmp_res_rot_var)
+        self.m_res_rot_mean_clip_port.set_all(res_rot_mean_clip)
 
 
 class MakePSFModelModule(ProcessingModule):
@@ -163,7 +334,7 @@ class MakePCABasisModule(ProcessingModule):
     """
 
     def __init__(self,
-                 name_in,
+                 name_in="make_pca_basis",
                  im_arr_in_tag="im_arr",
                  im_arr_out_tag="im_arr",
                  im_average_out_tag="im_ave",
@@ -175,7 +346,7 @@ class MakePCABasisModule(ProcessingModule):
         self._m_im_arr_in_port = self.add_input_port(im_arr_in_tag)
 
         # Outputs
-        self._m_im_arr_out_port = self.add_output_port(im_arr_out_tag)
+        self.m_im_arr_out_port = self.add_output_port(im_arr_out_tag)
         self._m_im_average_out_port = self.add_output_port(im_average_out_tag)
         self._m_basis_out_port = self.add_output_port(basis_out_tag)
 
@@ -203,7 +374,7 @@ class MakePCABasisModule(ProcessingModule):
 
         basis_pca_arr = V.reshape(V.shape[0], im_size[0], im_size[1])
 
-        self._m_im_arr_out_port.set_all(tmp_im_data, keep_attributes=True)
+        self.m_im_arr_out_port.set_all(tmp_im_data, keep_attributes=True)
         self._m_im_average_out_port.set_all(tmp_im_ave)
         self._m_basis_out_port.set_all(basis_pca_arr)
         self._m_basis_out_port.add_attribute(name="basis_type",
