1ce7aedf916ea9cca962d7f6e50e794c43245edb
majobodo <mbonse@uni-koblenz.de>
2016-07-31 19:11:57 +0200

    Finished End to End Pipeline

--- PynPoint/processing_modules/StackingAndSubsampling.py
+++ PynPoint/processing_modules/StackingAndSubsampling.py
@@ -27,44 +27,60 @@ class StackAndSubsetModule(ProcessingModule):
         if self.m_stacking in (None, False) and self.m_subset in (None, False):
             return
 
-        # get the data
-        tmp_data = self.m_image_in_port.get_all()
+        tmp_data_shape = self.m_image_in_port.get_shape()
 
         # check if the random subset is available
-        if tmp_data.shape[0] < self.m_subset:
+        if tmp_data_shape[0] < self.m_subset:
             raise ValueError("The number of images of the destination subset is bigger than the "
                              "number of images in the source.")
 
         # get attributes
         tmp_files = self.m_image_in_port.get_attribute("Used_Files")
+        number_of_images_per_file = self.m_image_in_port.get_attribute("NAXIS3")
         tmp_num_files = self.m_image_in_port.get_attribute("Num_Files")
         para_angles = self.m_image_in_port.get_attribute("NEW_PARA")
 
         # Do random subset first like in the old PynPoint
         if self.m_subset not in (None, False):
-            number_of_elements = tmp_data.shape[0] # number of images
+            number_of_elements = tmp_data_shape[0]  # number of images
 
             tmp_choice = np.random.choice(number_of_elements,
                                           self.m_subset,
                                           replace=False)
 
-            tmp_data = tmp_data[tmp_choice, :, :]
+            tmp_data = self.m_image_in_port[tmp_choice, :, :]
+            tmp_data_shape = tmp_data.shape
 
             if tmp_files is not None:
-                tmp_files = tmp_files[tmp_choice]
+                if number_of_images_per_file is None and \
+                                tmp_files == number_of_elements:
+                    tmp_files = tmp_files[tmp_choice]
+                elif number_of_images_per_file is not None:
+                    tmp_files = tmp_files[tmp_choice/number_of_images_per_file]
 
             tmp_num_files = self.m_subset
 
+            subset = True
+        else:
+            subset = False
+
         if self.m_stacking not in (False, None):
 
-            num_new = int(np.floor(float(tmp_data.shape[0])/float(self.m_stacking)))
+            num_new = int(np.floor(float(tmp_data_shape[0])/float(self.m_stacking)))
             para_new = np.zeros(num_new)
             im_arr_new = np.zeros([num_new,
-                                   tmp_data.shape[1],
-                                   tmp_data.shape[2]])
+                                   tmp_data_shape[1],
+                                   tmp_data_shape[2]])
             for i in range(0, num_new):
                 para_new[i] = para_angles[i*self.m_stacking:(i+1)*self.m_stacking].mean()
-                im_arr_new[i, ] = tmp_data[i*self.m_stacking:(i+1)*self.m_stacking, ].mean(axis=0)
+                if subset:
+                    im_arr_new[i, ] = tmp_data[i*self.m_stacking:(i+1)*self.m_stacking, ]\
+                        .mean(axis=0)
+                else:
+                    im_arr_new[i,] = self.m_image_in_port[i * self.m_stacking:(i + 1) * self.m_stacking, ] \
+                        .mean(axis=0)
+
+                print "loop " + str(i) + " of " + str(num_new)
 
             # Update for saving
             tmp_data = im_arr_new
@@ -75,6 +91,9 @@ class StackAndSubsetModule(ProcessingModule):
                                       keep_attributes=True)
 
         # Save Attributes
+        # copy old attributes
+        self.m_image_out_port.copy_attributes_from_input_port(self.m_image_in_port)
+
         self.m_image_out_port.add_attribute("NEW_PARA",
                                             para_angles,
                                             static=False)
@@ -86,3 +105,17 @@ class StackAndSubsetModule(ProcessingModule):
         self.m_image_out_port.add_attribute("Num_Files",
                                             tmp_num_files,
                                             static=True)
+
+        if self.m_stacking is None:
+            history_stacked = "Nothing Stacked and "
+        else:
+            history_stacked = "Stacked every " + str(self.m_stacking) + " and "
+
+        if self.m_subset is None:
+            history_subset = "no subset chosen"
+        else:
+            history_subset = "picked randomly " + str(self.m_subset) + " frames."
+
+        history = history_stacked + history_subset
+        self.m_image_out_port.add_history_information("Subset",
+                                                      history)
