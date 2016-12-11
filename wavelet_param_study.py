0e09bb1d64bed7f3efec25dc4a276f95f62281fb
majobodo <mbonse@uni-koblenz.de>
2016-12-11 16:55:09 +0100

    BLUESKY Fix

--- wavelet_param_study.py
+++ wavelet_param_study.py
@@ -19,26 +19,27 @@ pipeline = Pypeline("/Volumes/Seagate/Beta_Pic02/Working_files/",
                     "/Volumes/Seagate/Beta_Pic02/01_raw_part/",
                     "/Volumes/Seagate/Beta_Pic02/results")'''
 
+'''
 reading_data = ReadFitsCubesDirectory(name_in="Fits_reading",
                                       image_tag="00_raw_data")
-#pipeline.add_module(reading_data)
+pipeline.add_module(reading_data)
 
 reading_dark = ReadFitsCubesDirectory(name_in="Dark_reading",
                                       input_dir="/Volumes/Seagate/Beta_Pic02/00_Dark_and_Flat/Dark",
                                       image_tag="00_dark_arr")
-#pipeline.add_module(reading_dark)
+pipeline.add_module(reading_dark)
 
 reading_flat = ReadFitsCubesDirectory(name_in="Flat_reading",
                                       input_dir="/Volumes/Seagate/Beta_Pic02/00_Dark_and_Flat/Flat",
                                       image_tag="00_flat_arr")
-#pipeline.add_module(reading_flat)
+pipeline.add_module(reading_flat)
 
 # 01 NACO cutting
 
 cutting = CutTopTwoLinesModule(name_in="NACO_cutting",
                                image_in_tag="00_raw_data",
                                image_out_tag="01_raw_data_cut")
-#pipeline.add_module(cutting)
+pipeline.add_module(cutting)
 
 # 02 Dark and Flat Subtraction
 
@@ -52,16 +53,16 @@ flat_sub = FlatSubtractionModule(name_in="flat_subtraction",
                                  flat_in_tag="00_flat_arr",
                                  image_out_tag="02_dark_flat_sub")
 
-#pipeline.add_module(dark_sub)
-#pipeline.add_module(flat_sub)
+pipeline.add_module(dark_sub)
+pipeline.add_module(flat_sub)
 
 # 03 Bad Pixel
 
 bp_cleaning = BadPixelCleaningSigmaFilterModule(name_in="Bad_Pixel_filtering",
                                                 image_in_tag="02_dark_flat_sub",
                                                 image_out_tag="03_bad_pixel_clean")
-#pipeline.add_module(bp_cleaning)
-
+pipeline.add_module(bp_cleaning)
+'''
 # 04 Background Subtraction
 
 bg_subtraction = MeanBackgroundSubtractionModule(star_pos_shift=602,
