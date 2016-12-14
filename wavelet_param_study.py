3ae1979eb036d660426c8e86b4eb82da460a9f3b
majobodo <mbonse@uni-koblenz.de>
2016-12-14 15:25:06 +0100

    PSF Subtraction for BlueSky

--- wavelet_param_study.py
+++ wavelet_param_study.py
@@ -9,12 +9,15 @@ AngleCalculationModule, MeanBackgroundSubtractionModule, \
 StarExtractionModule, StarAlignmentModule, PSFSubtractionModule, \
 StackAndSubsetModule, WaveletTimeDenoisingModule, CwtWaveletConfiguration, DwtWaveletConfiguration
 
+from PynPoint.processing_modules.PSFSubtraction import MakePSFModelModule, CreateResidualsModule
+
 
 # 00 reading the data
 
 pipeline = Pypeline("/scratch/user/mbonse/Working_files/",
                     "/scratch/user/mbonse/Data/00_raw_Data/",
                     "/scratch/user/mbonse/results/")
+
 '''
 pipeline = Pypeline("/Volumes/Seagate/Beta_Pic02/Working_files/",
                     "/Volumes/Seagate/Beta_Pic02/01_raw_part/",
@@ -97,7 +100,8 @@ pipeline.add_module(alignment)
 
 angle_calc = AngleCalculationModule(name_in="angle_calculation",
                                     data_tag="06_star_arr_aligned2")
-pipeline.add_module(angle_calc)'''
+pipeline.add_module(angle_calc)
+
 
 # 07 Wavelet Analysis
 #wavelet = DwtWaveletConfiguration()
@@ -105,8 +109,10 @@ wavelet = CwtWaveletConfiguration(wavelet="dog",
                                   wavelet_order=2.0,
                                   keep_mean=True,
                                   resolution=0.2)
+'''
 
-k = 1
+wavelet_names = []
+# k = 1
 for j in [list(np.arange(0.0, 2.1, 0.2)),
           list(np.arange(2.2, 4.1, 0.2)),
           list(np.arange(4.2, 6.1, 0.2)),
@@ -114,19 +120,96 @@ for j in [list(np.arange(0.0, 2.1, 0.2)),
 
     wavelet_thresholds = j
 
-    wavelet_names = []
+    #wavelet_names = []
     for i in wavelet_thresholds:
         wavelet_names.append("07_wavelet_denoised_" + str(int(i)) + "_" + str(int((i % 1.0)*10)))
 
+    '''
     denoising = WaveletTimeDenoisingModule(wavelet_configuration=wavelet,
                                            name_in="wavelet_time_denoising" + str(k),
-                                           image_in_tag="06_star_arr_aligned",
+                                           image_in_tag="06_star_arr_aligned2",
                                            image_out_tag=wavelet_names,
                                            denoising_threshold=wavelet_thresholds,
                                            padding="const_mean",
                                            num_rows_in_memory=48)
     pipeline.add_module(denoising)
-    k += 1
+    k += 1'''
+
+# 08 PSF Subtraction and preparation
+
+'''
+wavelet_names = ["07_wavelet_denoised_0_0",
+                 "07_wavelet_denoised_4_0",
+                 "07_wavelet_denoised_8_0"]'''
+
+pca_numbers = range(1, 20, 1)
+pca_numbers.extend(range(25, 90, 5))
+
+#pca_numbers = range(1, 5)
+
+for denoising_result in wavelet_names:
+
+    psf_subtraction = PSFSubtractionModule(0,
+                                           name_in="PSF_sub_for" + denoising_result,
+                                           images_in_tag=denoising_result,
+                                           reference_in_tag=denoising_result,
+                                           res_mean_tag="08_res_mean_for_" + denoising_result +
+                                                        "_" + str(0).zfill(2),  # 0 PCAs
+                                           res_median_tag="08_res_median_for_" + denoising_result +
+                                                        "_" + str(0).zfill(2),  # 0 PCAs
+                                           res_rot_mean_clip_tag="08_res_rot_mean_clip_for_"
+                                                                 + denoising_result +
+                                                                 "_" + str(0).zfill(2),  # 0 PCAs
+                                           prep_tag="prep_data",
+                                           basis_out_tag="pca_basis",
+                                           image_ave_tag="im_ave",
+                                           cent_size=0.05)
+    pipeline.add_module(psf_subtraction)
+
+    writing = WriteAsSingleFitsFile("08_res_mean_for_" + denoising_result +
+                                    "_" + str(0).zfill(2) + ".fits",
+                                    name_in="writing_" + "08_res_mean_for_" + denoising_result +
+                                            "_" + str(0).zfill(2),
+                                    data_tag="08_res_mean_for_" + denoising_result +
+                                             "_" + str(0).zfill(2))
+    pipeline.add_module(writing)
+
+    for pca_number in pca_numbers:
+
+        tmp_make_psf_model = MakePSFModelModule(num=pca_number,
+                                                name_in="PSF_model_creation_for_" +
+                                                        denoising_result + "_" + str(pca_number),
+                                                im_arr_in_tag="prep_data",
+                                                basis_in_tag="pca_basis",
+                                                basis_average_in_tag="im_ave",
+                                                psf_basis_out_tag="tmp_psf_model")
+
+        tmp_residuals_module = \
+            CreateResidualsModule(name_in="Creating_residuals_for_" +
+                                          denoising_result + "_" + str(pca_number),
+                                  im_arr_in_tag="prep_data",
+                                  psf_im_in_tag="tmp_psf_model",
+                                  mask_in_tag="cent_mask_tag",
+                                  res_mean_tag="08_res_mean_for_" + denoising_result +
+                                               "_" + str(pca_number).zfill(2),
+                                  res_median_tag="08_res_median_for_" + denoising_result +
+                                               "_" + str(pca_number).zfill(2),
+                                  res_rot_mean_clip_tag="08_res_rot_mean_clip_for_" +
+                                                        denoising_result + "_" +
+                                                        str(pca_number).zfill(2),)
+
+        pipeline.add_module(tmp_make_psf_model)
+        pipeline.add_module(tmp_residuals_module)
+
+        # write out result as fits
+
+        writing = WriteAsSingleFitsFile("08_res_mean_for_" + denoising_result +
+                                        "_" + str(pca_number).zfill(2) + ".fits",
+                                        name_in="writing_" + "08_res_mean_for_" + denoising_result +
+                                                "_" + str(pca_number).zfill(2),
+                                        data_tag="08_res_mean_for_" + denoising_result +
+                                                 "_" + str(pca_number).zfill(2))
+        pipeline.add_module(writing)
 
 # xx run Pipeline
 
