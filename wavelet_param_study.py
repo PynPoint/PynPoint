8c610ebc3335643fff2bfc5735c5fce10bb2020d
majobodo <mbonse@uni-koblenz.de>
2016-12-15 14:41:05 +0100

    08 Klein 1-1.8

--- wavelet_param_study.py
+++ wavelet_param_study.py
@@ -14,7 +14,7 @@ from PynPoint.processing_modules.PSFSubtraction import MakePSFModelModule, Creat
 
 # 00 reading the data
 
-pipeline = Pypeline("/scratch/user/mbonse/Working_files/08_klein/",
+pipeline = Pypeline("/scratch/user/mbonse/Working_files/08_klein1/",
                     "/scratch/user/mbonse/Data/00_raw_Data/",
                     "/scratch/user/mbonse/results/")
 
@@ -143,35 +143,11 @@ wavelet_names = ["07_wavelet_denoised_0_0",
                  "07_wavelet_denoised_4_0",
                  "07_wavelet_denoised_8_0"]'''
 
-wavelet_names = ["07_wavelet_denoised_0_4",
-                 "07_wavelet_denoised_0_6",
-                 "07_wavelet_denoised_0_8",
-                 "07_wavelet_denoised_1_0",
+wavelet_names = ["07_wavelet_denoised_1_0",
                  "07_wavelet_denoised_1_2",
                  "07_wavelet_denoised_1_4",
                  "07_wavelet_denoised_1_6",
-                 "07_wavelet_denoised_1_8",
-                 "07_wavelet_denoised_2_0",
-                 "07_wavelet_denoised_2_2",
-                 "07_wavelet_denoised_2_4",
-                 "07_wavelet_denoised_2_6",
-                 "07_wavelet_denoised_2_8",
-                 "07_wavelet_denoised_3_0",
-                 "07_wavelet_denoised_3_2",
-                 "07_wavelet_denoised_3_4",
-                 "07_wavelet_denoised_3_6",
-                 "07_wavelet_denoised_3_8",
-                 "07_wavelet_denoised_4_0",
-                 "07_wavelet_denoised_4_2",
-                 "07_wavelet_denoised_4_4",
-                 "07_wavelet_denoised_4_6",
-                 "07_wavelet_denoised_4_8",
-                 "07_wavelet_denoised_5_0",
-                 "07_wavelet_denoised_5_2",
-                 "07_wavelet_denoised_5_4",
-                 "07_wavelet_denoised_5_6",
-                 "07_wavelet_denoised_5_8",
-                 "07_wavelet_denoised_6_0"]
+                 "07_wavelet_denoised_1_8"]
 
 pca_numbers = range(1, 20, 1)
 pca_numbers.extend(range(25, 90, 5))
