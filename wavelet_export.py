c3e1261bd28af6cd47612bfebe7d9d81604ecf0e
majobodo <mbonse@uni-koblenz.de>
2017-01-11 16:16:41 +0100

    HR8799 file update

--- wavelet_export.py
+++ wavelet_export.py
@@ -24,8 +24,8 @@ pipeline.add_module(saving1)
 # ---------------------------
 
 ff = {"07_wavelet_denoised_1_0" : "07_wavelet_denoised_1_0",
-      "07_wavelet_denoised_1_2" : "07_wavelet_denoised_1_2",
-      "07_wavelet_denoised_1_4" : "07_wavelet_denoised_1_4",
+      "07_wavelet_denoised_1_1" : "07_wavelet_denoised_1_2",
+      "07_wavelet_denoised_1_3" : "07_wavelet_denoised_1_4",
       "07_wavelet_denoised_1_6" : "07_wavelet_denoised_1_6",
       "07_wavelet_denoised_1_8" : "07_wavelet_denoised_1_8"}
 
@@ -40,9 +40,9 @@ pipeline.add_module(saving2)
 
 ff = {"07_wavelet_denoised_2_0" : "07_wavelet_denoised_2_0",
       "07_wavelet_denoised_2_2" : "07_wavelet_denoised_2_2",
-      "07_wavelet_denoised_2_4" : "07_wavelet_denoised_2_4",
+      "07_wavelet_denoised_2_3" : "07_wavelet_denoised_2_4",
       "07_wavelet_denoised_2_6" : "07_wavelet_denoised_2_6",
-      "07_wavelet_denoised_2_8" : "07_wavelet_denoised_2_8"}
+      "07_wavelet_denoised_2_7" : "07_wavelet_denoised_2_8"}
 
 saving3 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_03",
@@ -55,9 +55,9 @@ pipeline.add_module(saving3)
 
 ff = {"07_wavelet_denoised_3_0" : "07_wavelet_denoised_3_0",
       "07_wavelet_denoised_3_2" : "07_wavelet_denoised_3_2",
-      "07_wavelet_denoised_3_4" : "07_wavelet_denoised_3_4",
+      "07_wavelet_denoised_3_3" : "07_wavelet_denoised_3_4",
       "07_wavelet_denoised_3_6" : "07_wavelet_denoised_3_6",
-      "07_wavelet_denoised_3_8" : "07_wavelet_denoised_3_8"}
+      "07_wavelet_denoised_3_7" : "07_wavelet_denoised_3_8"}
 
 saving4 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_04",
@@ -71,8 +71,8 @@ pipeline.add_module(saving4)
 ff = {"07_wavelet_denoised_4_0" : "07_wavelet_denoised_4_0",
       "07_wavelet_denoised_4_2" : "07_wavelet_denoised_4_2",
       "07_wavelet_denoised_4_4" : "07_wavelet_denoised_4_4",
-      "07_wavelet_denoised_4_6" : "07_wavelet_denoised_4_6",
-      "07_wavelet_denoised_4_8" : "07_wavelet_denoised_4_8"}
+      "07_wavelet_denoised_4_5" : "07_wavelet_denoised_4_6",
+      "07_wavelet_denoised_4_7" : "07_wavelet_denoised_4_8"}
 
 saving5 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_05",
@@ -86,8 +86,8 @@ pipeline.add_module(saving5)
 ff = {"07_wavelet_denoised_5_0" : "07_wavelet_denoised_5_0",
       "07_wavelet_denoised_5_2" : "07_wavelet_denoised_5_2",
       "07_wavelet_denoised_5_4" : "07_wavelet_denoised_5_4",
-      "07_wavelet_denoised_5_6" : "07_wavelet_denoised_5_6",
-      "07_wavelet_denoised_5_8" : "07_wavelet_denoised_5_8"}
+      "07_wavelet_denoised_5_5" : "07_wavelet_denoised_5_6",
+      "07_wavelet_denoised_5_7" : "07_wavelet_denoised_5_8"}
 
 saving6 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_06",
