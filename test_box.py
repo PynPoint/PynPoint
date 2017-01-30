fc3d9e1f0923f1ced35efda652223795d4da5f9d
majobodo <mbonse@uni-koblenz.de>
2017-01-30 11:34:08 +0100

    HR8799 NR 01 - 06

--- test_box.py
+++ test_box.py
@@ -5,54 +5,26 @@ import operator
 
 folder_list = ["results_0_0/",
                "results_0_2/",
-               "results_0_4/",
-               "results_0_6/",
-               "results_0_8/",
-
                "results_1_0/",
-               "results_1_2/",
-               "results_1_4/",
-               "results_1_6/",
-               "results_1_8/",
+               "results_1_2/"]
 
-               "results_2_0/",
-               "results_2_2/",
-               "results_2_4/",
-               "results_2_6/",
-               "results_2_8/",
-
-               "results_3_0/",
-               "results_3_2/",
-               "results_3_4/",
-               "results_3_6/",
-               "results_3_8/",
-
-               "results_4_0/",
-               "results_4_2/",
-               "results_4_4/",
-               "results_4_6/",
-               "results_4_8/",
-
-               "results_5_0/",
-               "results_5_2/",
-               "results_5_4/",
-               "results_5_6/",
-               "results_5_8/"]
-
-planet_pos = (65, 63) # BP 29
-#planet_pos = (58.5, 67.5)  # HR8799 e 75 68
+#planet_pos = (65, 63) # BP 29
+planet_pos = (58.5, 67.5)  # HR8799 e
+#planet_pos = (47.0, 60.0)  # HR8799 f
 #planet_pos = (74, 67) # HR8799 d
 
-# shifts = np.linspace(-3.0, 3.0, num= 20) #
+shifts = np.linspace(-3.0, 3.0, num= 30) # HR8799 e
 
-shifts = np.linspace(-1.0, 1.0, num= 5)  # BP 29
+#shifts = np.linspace(-1.0, 1.0, num=5)  # BP 29
+#shifts = np.linspace(-2.0, 2.0, num=20)  # HR8799 f 75 68
 planet_shifts = [(planet_pos[0] + x, planet_pos[1] + y) for x in shifts for y in shifts]
 
-print "BP 29 small"
+print "HR8799"
 
 for tmploc in folder_list:
     print "------------- " + tmploc + "---------------"
-    tmp_location = "/Users/markusbonse/Desktop/BetaPic_29_small/" + tmploc
+    K = 0
+    tmp_location = "/Users/markusbonse/Desktop/" + tmploc
 
     last_planet_pos = planet_pos
 
@@ -87,7 +59,11 @@ for tmploc in folder_list:
 
         index, value = max(enumerate(snr_result_list), key=operator.itemgetter(1))
         #print planet_pos_list[index]
-        print value
+        print str(value)
+        K+=1
+        if K > 30:
+            K += 1
+
         #print index
 
         '''
