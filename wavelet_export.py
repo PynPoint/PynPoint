29888339b80a7dfae7939826ac02991124c401ea
majobodo <mbonse@uni-koblenz.de>
2017-03-30 22:10:18 +0200

    HR8799 L-band normalization - export and PSF sub

--- wavelet_export.py
+++ wavelet_export.py
@@ -2,44 +2,46 @@ from PynPoint import Pypeline
 from PynPoint.io_modules import Hdf5WritingModule
 
 
-pipeline = Pypeline("/scratch/user/mbonse/HR8799_2012_08_25/working_files/saves",
-                    "/scratch/user/mbonse/HR8799_2012_08_25/working_files/saves",
-                    "/scratch/user/mbonse/HR8799_2012_08_25/results")
+pipeline = Pypeline("/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/",
+                    "/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/",
+                    "/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/results")
 
 # ---------------------------
 
-ff = {"06_star_arr_aligned":"06_star_arr_aligned"}
+ff = {"06_star_arr_aligned" : "08_wavelet_denoised_0_0"}
 
 saving1 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_01",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized",
+                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/08_klein_0",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving1)
 
-'''
+
 # ---------------------------
 
-ff = {"07_wavelet_denoised_0_8" : "07_wavelet_denoised_0_8"}
+ff = {"07_wavelet_denoised_1_0" : "07_wavelet_denoised_1_0"}
 
 saving2 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_02",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_26_ND/working_files/08_klein_1",
+                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/08_klein_1",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving2)
 
 # ---------------------------
 
-ff = {"07_wavelet_denoised_1_0" : "07_wavelet_denoised_1_0"}
+ff = {"07_wavelet_denoised_2_0" : "07_wavelet_denoised_2_0"}
 
 saving3 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_03",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_26_ND/working_files/08_klein_2",
+                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/08_klein_2",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving3)
 
+
+'''
 # ---------------------------
 
 ff = {"07_wavelet_denoised_1_2" : "07_wavelet_denoised_1_2"}
