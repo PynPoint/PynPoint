85411e9ee72008f819fb064e5333fe15bf1135c4
majobodo <mbonse@uni-koblenz.de>
2017-03-30 21:56:19 +0200

    HR8799 L-band normalization - denoising

--- wavelet_denoising_study.py
+++ wavelet_denoising_study.py
@@ -3,9 +3,9 @@ import numpy as np
 from PynPoint.processing_modules import CwtWaveletConfiguration, WaveletTimeDenoisingModule
 
 
-pipeline = Pypeline("/scratch/user/mbonse/EPS_ERI_2015_small/Workplace/",
-                    "/scratch/user/mbonse/EPS_ERI_2015_small/Workplace/",
-                    "/scratch/user/mbonse/EPS_ERI_2015_small/results/")
+pipeline = Pypeline("/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized",
+                    "/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized",
+                    "/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/results")
 
 
 # 07 Wavelet Analysis
@@ -16,17 +16,17 @@ wavelet = CwtWaveletConfiguration(wavelet="dog",
 
 k = 1
 
-for j in [[1.0, 0.8, 1.2, 2.0], ]:
+for j in [[1.0, 2.0], ]:
 
     wavelet_thresholds = j
 
     wavelet_names = []
     for i in wavelet_thresholds:
-        wavelet_names.append("07_wavelet_denoised_" + str(int(i)) + "_" + str(int(round((i % 1.0)*10))))
+        wavelet_names.append("08_wavelet_denoised_" + str(int(i)) + "_" + str(int(round((i % 1.0)*10))))
 
     denoising = WaveletTimeDenoisingModule(wavelet_configuration=wavelet,
                                            name_in="wavelet_time_denoising" + str(k),
-                                           image_in_tag="bg_bp_cleaned_arr_cut",
+                                           image_in_tag="07_star_arr_normalized",
                                            image_out_tag=wavelet_names,
                                            denoising_threshold=wavelet_thresholds,
                                            padding="const_mean",
