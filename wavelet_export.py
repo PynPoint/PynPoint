b3bb4f6086862791e783f1f2847ec3d8576aa0c5
majobodo <mbonse@uni-koblenz.de>
2017-04-03 15:46:13 +0200

    Beta_Pic 26 - pre-processing export

--- wavelet_export.py
+++ wavelet_export.py
@@ -2,22 +2,32 @@ from PynPoint import Pypeline
 from PynPoint.io_modules import Hdf5WritingModule
 
 
-pipeline = Pypeline("/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/",
-                    "/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/",
-                    "/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/results")
+pipeline = Pypeline("/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace",
+                    "/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Data/00_raw_data",
+                    "/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Results")
 
 # ---------------------------
 
-ff = {"06_star_arr_aligned" : "08_wavelet_denoised_0_0"}
+ff = {"06_star_arr_aligned" : "06_star_arr_aligned"}
 
-saving1 = Hdf5WritingModule("PynPoint_database.hdf5",
+saving1 = Hdf5WritingModule("PynPoint_database_06.hdf5",
                             name_in="hdf5_writing_01",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/08_klein_0",
+                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Results",
+                            tag_dictionary=ff)
+
+pipeline.add_module(saving1)
+
+ff = {"07_star_arr_normalized" : "07_star_arr_normalized"}
+
+saving1 = Hdf5WritingModule("PynPoint_database_07.hdf5",
+                            name_in="hdf5_writing_02",
+                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Results",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving1)
 
 
+'''
 # ---------------------------
 
 ff = {"08_wavelet_denoised_1_0" : "08_wavelet_denoised_1_0"}
@@ -41,7 +51,7 @@ saving3 = Hdf5WritingModule("PynPoint_database.hdf5",
 pipeline.add_module(saving3)
 
 
-'''
+
 # ---------------------------
 
 ff = {"07_wavelet_denoised_1_2" : "07_wavelet_denoised_1_2"}
