9f54f2fe4a62d855add7607c6116d03274af2339
majobodo <mbonse@uni-koblenz.de>
2017-04-03 21:29:07 +0200

    Beta_Pic 26 - export and PSF subtraction

--- wavelet_export.py
+++ wavelet_export.py
@@ -8,33 +8,23 @@ pipeline = Pypeline("/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace",
 
 # ---------------------------
 
-ff = {"06_star_arr_aligned" : "06_star_arr_aligned"}
+ff = {"06_star_arr_aligned" : "08_wavelet_denoised_0_0"}
 
-saving1 = Hdf5WritingModule("PynPoint_database_06.hdf5",
+saving1 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_01",
-                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Results",
-                            tag_dictionary=ff)
-
-pipeline.add_module(saving1)
-
-ff = {"07_star_arr_normalized" : "07_star_arr_normalized"}
-
-saving1 = Hdf5WritingModule("PynPoint_database_07.hdf5",
-                            name_in="hdf5_writing_02",
-                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Results",
+                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace/08_klein_0",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving1)
 
 
-'''
 # ---------------------------
 
 ff = {"08_wavelet_denoised_1_0" : "08_wavelet_denoised_1_0"}
 
 saving2 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_02",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/08_klein_1",
+                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace/08_klein_1",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving2)
@@ -45,7 +35,7 @@ ff = {"08_wavelet_denoised_2_0" : "08_wavelet_denoised_2_0"}
 
 saving3 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_03",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25/working_files/normalized/08_klein_2",
+                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace/08_klein_2",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving3)
@@ -53,6 +43,7 @@ pipeline.add_module(saving3)
 
 
 # ---------------------------
+'''
 
 ff = {"07_wavelet_denoised_1_2" : "07_wavelet_denoised_1_2"}
 
