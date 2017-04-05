cce8b25743cf5823e0c13e9cf714146206a4c42f
majobodo <mbonse@uni-koblenz.de>
2017-04-05 15:12:12 +0200

    New soft threshold function for CWT shrinkage. Extra tests

--- test_box.py
+++ test_box.py
@@ -8,10 +8,11 @@ folder_list = ["0_0/",
                "2_0/"]
 
 # planet_pos = (65, 63) # BP 29
-planet_pos = (58.5, 67.5)  # HR8799 e
+# planet_pos = (58.5, 67.5)  # HR8799 e
 # planet_pos = (186.0, 142.0)  # HR8799 e keck
 # planet_pos = (47.0, 60.0)  # HR8799 f
 # planet_pos = (74, 67) # HR8799 d
+planet_pos = (65, 63) # BP 26
 
 shifts = np.linspace(-2.0, 2.0, num= 20) # HR8799 e
 
@@ -22,12 +23,12 @@ shifts = np.linspace(-2.0, 2.0, num= 20) # HR8799 e
 #shifts = np.linspace(-4.0, 4.0, num=20)  # HR8799 f 75 68
 planet_shifts = [(planet_pos[0] + x, planet_pos[1] + y) for x in shifts for y in shifts]
 
-print "HR8799e"
+print "Beta Pic 26"
 
 for tmploc in folder_list:
     print "------------- " + tmploc + "---------------"
 
-    tmp_location = "/Users/markusbonse/Desktop/results/" + tmploc
+    tmp_location = "/Users/markusbonse/Desktop/Results/" + tmploc
 
     last_planet_pos = planet_pos
 
@@ -54,13 +55,13 @@ for tmploc in folder_list:
 
         print str(value)
 
-        '''
+
         fpf_calculator.fpf_calculator(tmp_location + str(file),
                                       filetype="fits",
                                       planet_position=planet_pos_list[index],
                                       radius=4.0 / 2.0,
                                       method="exact",
                                       plot=True,
-                                      save=False)'''
+                                      save=False)
 
 
--- wavelet_denoising_study.py
+++ wavelet_denoising_study.py
@@ -14,15 +14,35 @@ wavelet = CwtWaveletConfiguration(wavelet="dog",
                                   keep_mean=True,
                                   resolution=0.2)
 
-k = 1
-
+# hard threshold
 for j in [[1.0, 2.0], ]:
 
     wavelet_thresholds = j
 
     wavelet_names = []
     for i in wavelet_thresholds:
-        wavelet_names.append("08_wavelet_denoised_" + str(int(i)) + "_" + str(int(round((i % 1.0)*10))))
+        wavelet_names.append("08_wavelet_denoised_hard_" + str(int(i)) + "_" + str(int(round((i % 1.0)*10))))
+
+    denoising = WaveletTimeDenoisingModule(wavelet_configuration=wavelet,
+                                           name_in="wavelet_time_denoising" + str(k),
+                                           image_in_tag="07_star_arr_normalized",
+                                           image_out_tag=wavelet_names,
+                                           denoising_threshold=wavelet_thresholds,
+                                           padding="zero",
+                                           median_filter=False,
+                                           threshold_function="hard",
+                                           num_rows_in_memory=30)
+    pipeline.add_module(denoising)
+
+
+# hard threshold + median
+for j in [[1.0, 2.0], ]:
+
+    wavelet_thresholds = j
+
+    wavelet_names = []
+    for i in wavelet_thresholds:
+        wavelet_names.append("08_wavelet_denoised_hard_median_" + str(int(i)) + "_" + str(int(round((i % 1.0)*10))))
 
     denoising = WaveletTimeDenoisingModule(wavelet_configuration=wavelet,
                                            name_in="wavelet_time_denoising" + str(k),
@@ -30,8 +50,52 @@ for j in [[1.0, 2.0], ]:
                                            image_out_tag=wavelet_names,
                                            denoising_threshold=wavelet_thresholds,
                                            padding="const_mean",
+                                           median_filter=True,
+                                           threshold_function="hard",
                                            num_rows_in_memory=30)
     pipeline.add_module(denoising)
-    k += 1
+
+
+# soft threshold
+for j in [[1.0, 2.0], ]:
+
+    wavelet_thresholds = j
+
+    wavelet_names = []
+    for i in wavelet_thresholds:
+        wavelet_names.append("08_wavelet_denoised_soft_" + str(int(i)) + "_" + str(int(round((i % 1.0)*10))))
+
+    denoising = WaveletTimeDenoisingModule(wavelet_configuration=wavelet,
+                                           name_in="wavelet_time_denoising" + str(k),
+                                           image_in_tag="07_star_arr_normalized",
+                                           image_out_tag=wavelet_names,
+                                           denoising_threshold=wavelet_thresholds,
+                                           padding="const_mean",
+                                           median_filter=False,
+                                           threshold_function="soft",
+                                           num_rows_in_memory=30)
+    pipeline.add_module(denoising)
+
+
+# soft threshold + median
+for j in [[1.0, 2.0], ]:
+
+    wavelet_thresholds = j
+
+    wavelet_names = []
+    for i in wavelet_thresholds:
+        wavelet_names.append("08_wavelet_denoised_soft_median_" + str(int(i)) + "_" + str(int(round((i % 1.0)*10))))
+
+    denoising = WaveletTimeDenoisingModule(wavelet_configuration=wavelet,
+                                           name_in="wavelet_time_denoising" + str(k),
+                                           image_in_tag="07_star_arr_normalized",
+                                           image_out_tag=wavelet_names,
+                                           denoising_threshold=wavelet_thresholds,
+                                           padding="const_mean",
+                                           median_filter=True,
+                                           threshold_function="soft",
+                                           num_rows_in_memory=30)
+    pipeline.add_module(denoising)
+
 
 pipeline.run()
