03619c3706f6d56ffcc72baa46929453a4b695ec
majobodo <mbonse@uni-koblenz.de>
2017-04-06 10:28:43 +0200

    soft and hard PSF and export

--- wavelet_export.py
+++ wavelet_export.py
@@ -8,7 +8,9 @@ pipeline = Pypeline("/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace",
 
 # ---------------------------
 
-ff = {"06_star_arr_aligned" : "08_wavelet_denoised_0_0"}
+ff = {"06_star_arr_aligned" : "08_wavelet_denoised_0_0",
+      "08_wavelet_denoised_hard_1_0" : "08_wavelet_denoised_hard_1_0",
+      "08_wavelet_denoised_hard_2_0" : "08_wavelet_denoised_hard_2_0"}
 
 saving1 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_01",
@@ -20,7 +22,8 @@ pipeline.add_module(saving1)
 
 # ---------------------------
 
-ff = {"08_wavelet_denoised_1_0" : "08_wavelet_denoised_1_0"}
+ff = {"08_wavelet_denoised_hard_median_1_0" : "08_wavelet_denoised_hard_median_1_0",
+      "08_wavelet_denoised_hard_median_2_0" : "08_wavelet_denoised_hard_median_2_0"}
 
 saving2 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_02",
@@ -31,7 +34,8 @@ pipeline.add_module(saving2)
 
 # ---------------------------
 
-ff = {"08_wavelet_denoised_2_0" : "08_wavelet_denoised_2_0"}
+ff = {"08_wavelet_denoised_soft_1_0" : "08_wavelet_denoised_soft_1_0",
+      "08_wavelet_denoised_soft_2_0" : "08_wavelet_denoised_soft_2_0"}
 
 saving3 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_03",
@@ -43,17 +47,18 @@ pipeline.add_module(saving3)
 
 
 # ---------------------------
-'''
 
-ff = {"07_wavelet_denoised_1_2" : "07_wavelet_denoised_1_2"}
+
+ff = {"08_wavelet_denoised_soft_median_1_0" : "08_wavelet_denoised_soft_median_1_0",
+      "08_wavelet_denoised_soft_median_2_0" : "08_wavelet_denoised_soft_median_2_0"}
 
 saving4 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_04",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_26_ND/working_files/08_klein_3",
+                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace/08_klein_3",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving4)
 
-# ---------------------------'''
+# ---------------------------
 
 pipeline.run()
