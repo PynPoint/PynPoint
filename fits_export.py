a96656dbf4ff39ddfc45beafa71960a75b7f6279
majobodo <mbonse@uni-koblenz.de>
2017-04-07 17:44:20 +0200

    Mirror padding for COI

--- fits_export.py
+++ fits_export.py
@@ -1,20 +1,14 @@
 from PynPoint import Pypeline
 from PynPoint.io_modules import WriteAsSingleFitsFile
 
-pipeline = Pypeline("/Volumes/Seagate/ETH/normalized",
-                    "/Volumes/Seagate/ETH/normalized",
-                    "/Volumes/Seagate/ETH/normalized/results")
+pipeline = Pypeline("/Users/markusbonse/Desktop",
+                    "/Users/markusbonse/Desktop",
+                    "/Users/markusbonse/Desktop")
 
-writing1 = WriteAsSingleFitsFile("no_wavelets.fits",
+writing1 = WriteAsSingleFitsFile("soft_wavelets.fits",
                                  name_in="fits_writing_1",
-                                 data_tag="07_star_arr_normalized")
+                                 data_tag="08_wavelet_denoised_soft_median_1_0")
 
 pipeline.add_module(writing1)
-
-writing2 = WriteAsSingleFitsFile("wavelets.fits",
-                                 name_in="fits_writing_2",
-                                 data_tag="08_wavelet_denoised_1_0")
-
-pipeline.add_module(writing2)
 
 pipeline.run()
