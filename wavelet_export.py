7629ab1592f0dbb29ee6e6f55c8add9369f68552
majobodo <mbonse@uni-koblenz.de>
2017-04-10 13:23:41 +0200

    HR8799 09 and export

--- wavelet_export.py
+++ wavelet_export.py
@@ -9,35 +9,37 @@ pipeline = Pypeline("/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace",
 # ---------------------------
 
 ff = {"06_star_arr_aligned" : "06_star_arr_aligned",
-      "07_star_arr_normalized": "07_star_arr_normalized"}
+      "08_wavelet_denoised_mirror_hard_1_0": "08_wavelet_denoised_mirror_hard_1_0",
+      "08_wavelet_denoised_mirror_hard_median_1_0" : "08_wavelet_denoised_mirror_hard_median_1_0"}
 
 saving1 = Hdf5WritingModule("PynPoint_database_06_07.hdf5",
                             name_in="hdf5_writing_01",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace/saves",
+                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace/08_klein_0",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving1)
 
-'''
 
 # ---------------------------
 
-ff = {"08_wavelet_denoised_mirror_hard_median_1_0" : "08_wavelet_denoised_mirror_hard_median_1_0"}
+ff = {"08_wavelet_denoised_mirror_soft_1_0" : "08_wavelet_denoised_mirror_soft_1_0",
+      "08_wavelet_denoised_mirror_soft_median_1_0" : "08_wavelet_denoised_mirror_soft_median_1_0"}
 
 saving2 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_02",
-                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace/08_klein_1",
+                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace/08_klein_1",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving2)
 
 # ---------------------------
 
-ff = {"08_wavelet_denoised_mirror_soft_1_0" : "08_wavelet_denoised_mirror_soft_1_0"}
+ff = {"08_wavelet_denoised_zero_hard_1_0" : "08_wavelet_denoised_zero_hard_1_0",
+      "08_wavelet_denoised_zero_hard_median_1_0" : "08_wavelet_denoised_zero_hard_median_1_0"}
 
 saving3 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_03",
-                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace/08_klein_2",
+                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace/08_klein_2",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving3)
@@ -47,15 +49,16 @@ pipeline.add_module(saving3)
 # ---------------------------
 
 
-ff = {"08_wavelet_denoised_mirror_soft_median_1_0" : "08_wavelet_denoised_mirror_soft_median_1_0"}
+ff = {"08_wavelet_denoised_zero_soft_1_0" : "08_wavelet_denoised_zero_soft_1_0",
+      "08_wavelet_denoised_zero_soft_median_1_0" : "08_wavelet_denoised_zero_soft_median_1_0"}
 
 saving4 = Hdf5WritingModule("PynPoint_database.hdf5",
                             name_in="hdf5_writing_04",
-                            output_dir="/scratch/user/mbonse/Beta_Pic_2009_12_26_norm/Workplace/08_klein_3",
+                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace/08_klein_3",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving4)
-'''
+
 # ---------------------------
 
 pipeline.run()
