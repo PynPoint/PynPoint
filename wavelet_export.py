6d752020d446f178b0b50b702529555a6e9702b0
majobodo <mbonse@uni-koblenz.de>
2017-04-13 17:01:56 +0200

    Beta Pic 29 export 06 07

--- wavelet_export.py
+++ wavelet_export.py
@@ -2,24 +2,23 @@ from PynPoint import Pypeline
 from PynPoint.io_modules import Hdf5WritingModule
 
 
-pipeline = Pypeline("/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace",
-                    "/scratch/user/mbonse/HR8799_2012_08_25_norm/Data/00_raw_data",
-                    "/scratch/user/mbonse/HR8799_2012_08_25_norm/Results")
+pipeline = Pypeline("/scratch/user/mbonse/Beta_Pic_2009_12_29_norm/Workplace",
+                    "/scratch/user/mbonse/Beta_Pic_2009_12_29_norm/Data/00_raw_Data",
+                    "/scratch/user/mbonse/Beta_Pic_2009_12_29_norm/Results")
 
 # ---------------------------
 
 ff = {"06_star_arr_aligned" : "06_star_arr_aligned",
-      "08_wavelet_denoised_mirror_hard_1_0": "08_wavelet_denoised_mirror_hard_1_0",
-      "08_wavelet_denoised_mirror_hard_median_1_0" : "08_wavelet_denoised_mirror_hard_median_1_0"}
+      "07_star_arr_normalized": "07_star_arr_normalized"}
 
 saving1 = Hdf5WritingModule("PynPoint_database_06_07.hdf5",
                             name_in="hdf5_writing_01",
-                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace/08_klein_0",
+                            output_dir="/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace",
                             tag_dictionary=ff)
 
 pipeline.add_module(saving1)
 
-
+'''
 # ---------------------------
 
 ff = {"08_wavelet_denoised_mirror_soft_1_0" : "08_wavelet_denoised_mirror_soft_1_0",
@@ -57,7 +56,7 @@ saving4 = Hdf5WritingModule("PynPoint_database.hdf5",
                             output_dir="/scratch/user/mbonse/HR8799_2012_08_25_norm/Workplace/08_klein_3",
                             tag_dictionary=ff)
 
-pipeline.add_module(saving4)
+pipeline.add_module(saving4)'''
 
 # ---------------------------
 
