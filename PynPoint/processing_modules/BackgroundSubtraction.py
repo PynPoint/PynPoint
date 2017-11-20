bb049133dfc70a017887a9cb9993dab741623c3e
Tomas Stolker <tomas.stolker@gmail.com>
2017-11-20 17:51:12 +0100

    End-to-end test case, SimpleBackground with NAXIS3

--- PynPoint/processing_modules/BackgroundSubtraction.py
+++ PynPoint/processing_modules/BackgroundSubtraction.py
@@ -1,4 +1,10 @@
+"""
+Modules with background subtraction routines.
+"""
+
 import numpy as np
+import warnings
+import sys
 from PynPoint.core.Processing import ProcessingModule
 
 
@@ -6,9 +12,9 @@ class MeanBackgroundSubtractionModule(ProcessingModule):
 
     def __init__(self,
                  star_pos_shift,
-                 name_in = "mean_background_subtraction",
-                 image_in_tag = "im_arr",
-                 image_out_tag = "bg_cleaned_arr"):
+                 name_in="mean_background_subtraction",
+                 image_in_tag="im_arr",
+                 image_out_tag="bg_cleaned_arr"):
 
         super(MeanBackgroundSubtractionModule, self).__init__(name_in)
 
@@ -103,57 +109,117 @@ class MeanBackgroundSubtractionModule(ProcessingModule):
 
         self.m_image_out_port.copy_attributes_from_input_port(self.m_image_in_port)
 
-        self.m_image_out_port.add_history_information("background sub",
+        self.m_image_out_port.add_history_information("Background",
                                                       "mean subtraction")
 
         self.m_image_out_port.close_port()
 
 
 class SimpleBackgroundSubtractionModule(ProcessingModule):
+    """
+    Module for simple background subtraction, only applicable for dithered data.
+    """
 
     def __init__(self,
                  star_pos_shift,
                  name_in="background_subtraction",
-                 image_in_tag = "im_arr",
-                 image_out_tag = "bg_cleaned_arr"):
+                 image_in_tag="im_arr",
+                 image_out_tag="bg_cleaned_arr"):
+        """
+        Constructor of SimpleBackgroundSubtractionModule.
+
+        :param star_pos_shift: Frame number offset for the background used for subtraction.
+                               Typically equal to the number of frames per dither location.
+                               If set to *None*, the (non-static) NAXIS3 values from the
+                               FITS headers will be used.
+        :type star_pos_shift: int
+        :param name_in: Unique name of the module instance.
+        :type name_in: str
+        :param image_in_tag: Tag of the database entry that is read as input.
+        :type image_in_tag: str
+        :param image_out_tag: Tag of the database entry that is written as output.
+        :type image_out_tag: str
+        :return: None
+        """
 
         super(SimpleBackgroundSubtractionModule, self).__init__(name_in)
 
-        # add Ports
         self.m_image_in_port = self.add_input_port(image_in_tag)
         self.m_image_out_port = self.add_output_port(image_out_tag)
 
         self.m_star_prs_shift = star_pos_shift
 
     def run(self):
+        """
+        Run method of the module. Simple background subtraction which uses a constant index offset. 
+
+        :return: None
+        """
+
+        # Use NAXIS3 values if star_pos_shift is None
+        if self.m_star_prs_shift is None:
+            self.m_star_prs_shift = self.m_image_in_port.get_attribute("NAXIS3")
 
         number_of_frames = self.m_image_in_port.get_shape()[0]
 
-        # first subtraction is used to set up the output port array
-        tmp_res = self.m_image_in_port[0] - \
-                  self.m_image_in_port[(0 + self.m_star_prs_shift) % number_of_frames]
+        # First subtraction to set up the output port array
+        if isinstance(self.m_star_prs_shift, np.ndarray):
+            # Modulo is needed when the index offset exceeds the total number of frames
+            tmp_res = self.m_image_in_port[0] - \
+                      self.m_image_in_port[(0 + self.m_star_prs_shift[0]) % number_of_frames]
+
+        else:
+            tmp_res = self.m_image_in_port[0] - \
+                      self.m_image_in_port[(0 + self.m_star_prs_shift) % number_of_frames]
 
         if self.m_image_in_port.tag == self.m_image_out_port.tag:
             self.m_image_out_port[0] = tmp_res
         else:
             self.m_image_out_port.set_all(tmp_res, data_dim=3)
 
-        # process with the rest of the data
-        for i in range(1, number_of_frames):
-            tmp_res = self.m_image_in_port[i] - \
-                      self.m_image_in_port[(i + self.m_star_prs_shift) % number_of_frames]
+        # Background subtraction of the rest of the data
+        if isinstance(self.m_star_prs_shift, np.ndarray):
+            frame_count = 1
+            for i, naxis_three in enumerate(self.m_star_prs_shift):
+                for j in range(naxis_three):
+                    if i == 0 and j == 0:
+                        continue
+                    else:
+                        # TODO This will cause problems if the NAXIS3 value decreases and the
+                        # amount of dithering positions is small, e.g. two dithering positions
+                        # with subsequent NAXIS3 values of 20, 10, and 10. Also, the modulo does
+                        # not guarentee to give a correct background frame. With the current
+                        # implementation some background frames are used twice when the NAXIS3
+                        # values changes which is not necessarily wrong but something to be
+                        # aware of.
+                        if j == 0 and i < np.size(self.m_star_prs_shift)-1 and \
+                                  self.m_star_prs_shift[i+1] > naxis_three:
+                            warnings.warn("A small number of dither positions and variations of "
+                                          "NAXIS3 may give incorrect results with the current "
+                                          "implementation.")
 
-            if self.m_image_in_port.tag == self.m_image_out_port.tag:
-                self.m_image_out_port[i] = tmp_res
-            else:
-                self.m_image_out_port.append(tmp_res)
+                        tmp_res = self.m_image_in_port[frame_count] - \
+                                  self.m_image_in_port[(frame_count + naxis_three) % number_of_frames]
+
+                    frame_count += 1
+
+                    if self.m_image_in_port.tag == self.m_image_out_port.tag:
+                        self.m_image_out_port[i] = tmp_res
+                    else:
+                        self.m_image_out_port.append(tmp_res)
+        else:
+            for i in range(1, number_of_frames):
+                tmp_res = self.m_image_in_port[i] - \
+                          self.m_image_in_port[(i + self.m_star_prs_shift) % number_of_frames]
+
+                if self.m_image_in_port.tag == self.m_image_out_port.tag:
+                    self.m_image_out_port[i] = tmp_res
+                else:
+                    self.m_image_out_port.append(tmp_res)
 
         self.m_image_out_port.copy_attributes_from_input_port(self.m_image_in_port)
 
-        self.m_image_out_port.add_history_information("background sub",
-                                                      "simple frame - frame")
+        self.m_image_out_port.add_history_information("Background",
+                                                      "simple subtraction")
 
         self.m_image_out_port.close_port()
-
-
-
