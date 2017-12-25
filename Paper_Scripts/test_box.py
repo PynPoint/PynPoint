c226bf9bc54dca6cc33a68fe5334ace44465d099
majobodo <mbonse@uni-koblenz.de>
2017-12-25 21:05:47 +0100

    Some more tests

--- Paper_Scripts/test_box.py
+++ Paper_Scripts/test_box.py
@@ -10,29 +10,9 @@ pipeline = Pypeline("/scratch/user/mbonse/FastPca",
                     "/scratch/user/mbonse/FastPca",
                     "/scratch/user/mbonse/FastPca")
 
-
-'''
-wv = CwtWaveletConfiguration(wavelet="dog",
-                             wavelet_order=2,
-                             keep_mean=False,
-                             resolution=0.5)
-
-time_denoising = WaveletTimeDenoisingModule(wv,
-                                            name_in="time_denoising",
-                                            image_in_tag="10_stacked",
-                                            image_out_tag="star_arr_denoised",
-                                            denoising_threshold=1.0,
-                                            padding="zero",
-                                            median_filter=True,
-                                            threshold_function="soft")
-
-pipeline.add_module(time_denoising)
-
-pipeline.run_module("time_denoising")'''
-
 from PynPoint.processing_modules import PSFSubtractionPCA
 
-psf_subtraction = PSFSubtractionPCA(range(1),
+psf_subtraction = PSFSubtractionPCA(range(400),
                                     name_in="PSF_subtraction",
                                     images_in_tag="10_stacked",
                                     reference_in_tag="10_stacked")
