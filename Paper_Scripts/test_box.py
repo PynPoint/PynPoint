94f1779706616c87fa27a707b7d91e8cd79813b3
majobodo <mbonse@uni-koblenz.de>
2017-12-26 21:36:22 +0000

    Last tests on more data

--- Paper_Scripts/test_box.py
+++ Paper_Scripts/test_box.py
@@ -12,7 +12,7 @@ pipeline = Pypeline("/scratch/user/mbonse/FastPca",
 
 from PynPoint.processing_modules import PSFSubtractionPCA
 
-psf_subtraction = PSFSubtractionPCA(range(500),
+psf_subtraction = PSFSubtractionPCA(20,
                                     name_in="PSF_subtraction",
                                     images_in_tag="10_stacked",
                                     reference_in_tag="10_stacked")
