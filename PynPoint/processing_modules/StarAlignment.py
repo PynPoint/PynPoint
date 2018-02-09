d670bfa4878ea52fc3598ec757826e3a36ee1fed
Tomas Stolker <tomas.stolker@gmail.com>
2018-02-09 13:54:37 +0100

    Single normalization value for both images and references

--- PynPoint/processing_modules/StarAlignment.py
+++ PynPoint/processing_modules/StarAlignment.py
@@ -211,11 +211,10 @@ class StarAlignmentModule(ProcessingModule):
 
             offset = np.array([0.0, 0.0])
             for i in range(self.m_num_references):
-                ref_norm = np.amax(np.abs(ref_images[i, ]))
-                im_norm = np.amax(np.abs(image_in[i, ]))
+                norm = max(np.amax(np.abs(ref_images[i, ])), np.amax(np.abs(image_in[i, ])))
 
-                tmp_offset, _, _ = register_translation(ref_images[i, ]/ref_norm,
-                                                        image_in/im_norm,
+                tmp_offset, _, _ = register_translation(ref_images[i, ]/norm,
+                                                        image_in/norm,
                                                         upsample_factor=self.m_accuracy)
                 offset += tmp_offset
 
