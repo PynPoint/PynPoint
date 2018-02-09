02a8b66af8871df227f0d1f79e04d4d6af069f99
Tomas Stolker <tomas.stolker@gmail.com>
2018-02-09 11:39:17 +0100

    Added SNRModule

--- PynPoint/processing_modules/__init__.py
+++ PynPoint/processing_modules/__init__.py
@@ -16,5 +16,5 @@ from PSFsubPreparation import PSFdataPreparation
 from TimeDenoising import CwtWaveletConfiguration, DwtWaveletConfiguration, \
     WaveletTimeDenoisingModule, TimeNormalizationModule
 from FrameSelection import RemoveFramesModule, FrameSelectionModule
-from FluxAndPosition import FakePlanetModule, SimplexMinimizationModule
+from FluxAndPosition import FakePlanetModule, SimplexMinimizationModule, SNRModule
 from DetectionLimits import ContrastModule
