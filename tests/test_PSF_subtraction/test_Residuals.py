3a4e456ef52c49ba5d9c1b53bdb08cd0b9bc27f4
Tomas Stolker <tomas.stolker@gmail.com>
2018-03-13 14:19:21 +0100

    New implementation of the mask in PSFpreparationModule

--- tests/test_PSF_subtraction/test_Residuals.py
+++ tests/test_PSF_subtraction/test_Residuals.py
@@ -92,24 +92,24 @@ class TestResidual(object):
 
     def setup(self):
         self.test_data_dir = os.path.dirname(__file__) + '/test_data/'
-        self.basis = PynPoint.basis.create_wdir(self.test_data_dir, resize=-1, ran_sub=False, cent_size=0.2)
-        self.images = PynPoint.images.create_wdir(self.test_data_dir, resize=-1, ran_sub=False, cent_size=0.2)
+        self.basis = PynPoint.basis.create_wdir(self.test_data_dir, resize=None, ran_sub=False, cent_size=0.2)
+        self.images = PynPoint.images.create_wdir(self.test_data_dir, resize=None, ran_sub=False, cent_size=0.2)
         self.res = PynPoint.residuals.create_winstances(self.images, self.basis)
         self.num_files = self.images.im_arr.shape[0]
 
     def test_res_rot_mean(self):
         assert self.res.res_arr(1).shape == (self.num_files, 146, 146)
         assert np.allclose(self.res.res_arr(1).mean(), -6.99879052984e-21, rtol=limit)
-        assert np.allclose(self.res.res_arr(1).var(), 6.04791954677e-08, rtol=limit)
+        assert np.allclose(self.res.res_arr(1).var(), 9.017632410173216e-08, rtol=limit)
         assert self.res.res_rot(1).shape == (self.num_files, 146, 146)
-        assert np.allclose(self.res.res_rot(1).mean(), -1.98001052939e-09, rtol=limit)
+        assert np.allclose(self.res.res_rot(1).mean(), -2.3283496917978495e-08, rtol=limit)
         assert self.res.res_rot_mean(1).shape == (146, 146)
-        assert np.allclose(self.res.res_rot_mean(1).mean(), -1.98001052939e-09, rtol=limit)
+        assert np.allclose(self.res.res_rot_mean(1).mean(), -2.3283496917978495e-08, rtol=limit)
         assert self.res.res_rot_mean_clip(1).shape == (146, 146)
         assert self.res.res_rot_var(1).shape == (146, 146)
-        assert np.allclose(self.res.res_rot_var(1).mean(), 2.39710290476e-07, rtol=limit)
+        assert np.allclose(self.res.res_rot_var(1).mean(), 3.2090995369569343e-07, rtol=limit)
         assert self.res._psf_im(1).shape == (self.num_files, 146, 146)
-        assert np.allclose(self.res._psf_im(1).mean(), 0.000293242276843, rtol=limit)
+        assert np.allclose(self.res._psf_im(1).mean(), 0.0004695346933652781, rtol=limit)
 
     def test_residuals_save_restore(self):
         temp_file = os.path.join(self.test_data_dir, 'tmp_res.hdf5')
