77401fa8b21231e0dc5aa2efefeefe8f10e3b770
gcugno <gcugno@student.ethz.ch>
2018-03-20 15:42:31 +0100

    Update PSFpreparation.py

--- PynPoint/ProcessingModules/PSFpreparation.py
+++ PynPoint/ProcessingModules/PSFpreparation.py
@@ -397,9 +397,9 @@ class SortParangModule(ProcessingModule):
 
 
 class SDIPreparationModule(ProcessingModule):
-   """
-   Module for preparing continuum frames for SDI subtraction.
-   """
+    """
+    Module for preparing continuum frames for SDI subtraction.
+    """
 
     def __init__(self,
                  line_wvl,
@@ -409,27 +409,27 @@ class SDIPreparationModule(ProcessingModule):
                  name_in="SDI_preparation",
                  image_in_tag="im_arr",
                  image_out_tag="im_arr_SDI"):
-       """
-       Constructor of SDIPreparationModule.
+        """
+        Constructor of SDIPreparationModule.
 
-       :param line_wvl: central wavelength of the line filter.
-       :type line_wvl: float
-       :param cnt_wvl: central wavelength of the continuum filter.
-       :type cnt_wvl: float
-       :param line_width: equivalent width of the line filter.
-       :type line_width: float
-       :param cnt_width: equivalent width of the continuum filter.
-       :type cnt_width: float
-       :param name_in: Unique name of the module instance.
-       :type name_in: str
-       :param image_in_tag: Tag of the database entry that is read as input.
-       :type image_in_tag: str
-       :param image_out_tag: Tag of the database entry that is written as output. Should be
-                             different from *image_in_tag*.
-       :type image_out_tag: str
+        :param line_wvl: central wavelength of the line filter.
+        :type line_wvl: float
+        :param cnt_wvl: central wavelength of the continuum filter.
+        :type cnt_wvl: float
+        :param line_width: equivalent width of the line filter.
+        :type line_width: float
+        :param cnt_width: equivalent width of the continuum filter.
+        :type cnt_width: float
+        :param name_in: Unique name of the module instance.
+        :type name_in: str
+        :param image_in_tag: Tag of the database entry that is read as input.
+        :type image_in_tag: str
+        :param image_out_tag: Tag of the database entry that is written as output. Should be
+                              different from *image_in_tag*.
+        :type image_out_tag: str
 
-       :return: None
-       """
+        :return: None
+        """
 
         super(SDIPreparationModule, self).__init__(name_in)
 
@@ -444,12 +444,12 @@ class SDIPreparationModule(ProcessingModule):
         self.m_cnt_out_tag = image_out_tag
 
     def run(self):
-       """
-       Run method of the module. Normalizes for different filter widths, upscales the images
-       to align PSF patterns and crops them to be of the same dimension as before.
+        """
+        Run method of the module. Normalizes for different filter widths, upscales the images
+        to align PSF patterns and crops them to be of the same dimension as before.
 
-       :return: None
-       """
+        :return: None
+        """
 
         self.m_image_out_port.del_all_data()
         self.m_image_out_port.del_all_attributes()
