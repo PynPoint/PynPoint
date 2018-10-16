06ff686ce2b501cfc891d743cccd90f2a4599bf3
Anna Boehle <anna.boehle@gmail.com>
2018-10-16 15:43:45 +0200

    Moved warnings to simplex and contrast curve modules, added conditions to fake_planet to cover all cases of psf/image size.

--- PynPoint/ProcessingModules/DetectionLimits.py
+++ PynPoint/ProcessingModules/DetectionLimits.py
@@ -162,6 +162,10 @@ class ContrastCurveModule(ProcessingModule):
 
         self.m_aperture /= pixscale
 
+        if psf.ndim == 3 and psf.shape[0] != images.shape[0]:
+            warnings.warn('The number of frames in psf_in_tag does not match with the number of '
+                          'frames in image_in_tag. Using the mean of psf_in_tag as PSF template.')
+
         center = np.array([images.shape[2]/2., images.shape[1]/2.])
 
         pos_r = np.arange(self.m_separation[0]/pixscale,
