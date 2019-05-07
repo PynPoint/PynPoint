e228b4a9b056419b13c15390c52d2e8ced37d91d
Tomas Stolker <tomas.stolker@phys.ethz.ch>
2019-05-07 21:19:47 +0200

    test cases

--- tests/test_processing/test_darkflat.py
+++ tests/test_processing/test_darkflat.py
@@ -52,9 +52,6 @@ class TestDarkAndFlatCalibration:
 
     def test_dark_calibration(self):
 
-        database = h5py.File(self.test_dir+'PynPoint_database.hdf5', 'a')
-        database['config'].attrs['CPU'] = 1
-
         module = DarkCalibrationModule(name_in="dark",
                                        image_in_tag="images",
                                        dark_in_tag="dark",
@@ -63,30 +60,12 @@ class TestDarkAndFlatCalibration:
         self.pipeline.add_module(module)
         self.pipeline.run_module("dark")
 
-        database = h5py.File(self.test_dir+'PynPoint_database.hdf5', 'a')
-        database['config'].attrs['CPU'] = 4
-
-        module = DarkCalibrationModule(name_in="dark_multi",
-                                       image_in_tag="images",
-                                       dark_in_tag="dark",
-                                       image_out_tag="dark_cal_multi")
-
-        self.pipeline.add_module(module)
-        self.pipeline.run_module("dark_multi")
-
         data = self.pipeline.get_data("dark_cal")
         assert np.allclose(data[0, 10, 10], 0.0001881700200690493, rtol=limit, atol=0.)
         assert np.allclose(np.mean(data), 3.137393482985464e-07, rtol=limit, atol=0.)
         assert data.shape == (10, 100, 100)
 
-        data_multi = self.pipeline.get_data("dark_cal_multi")
-        assert np.allclose(data, data_multi, rtol=limit, atol=0.)
-        assert data.shape == data_multi.shape
-
     def test_flat_calibration(self):
-
-        database = h5py.File(self.test_dir+'PynPoint_database.hdf5', 'a')
-        database['config'].attrs['CPU'] = 1
 
         module = FlatCalibrationModule(name_in="flat",
                                        image_in_tag="dark_cal",
@@ -96,22 +75,7 @@ class TestDarkAndFlatCalibration:
         self.pipeline.add_module(module)
         self.pipeline.run_module("flat")
 
-        database = h5py.File(self.test_dir+'PynPoint_database.hdf5', 'a')
-        database['config'].attrs['CPU'] = 4
-
-        module = FlatCalibrationModule(name_in="flat_multi",
-                                       image_in_tag="dark_cal",
-                                       flat_in_tag="flat",
-                                       image_out_tag="flat_cal_multi")
-
-        self.pipeline.add_module(module)
-        self.pipeline.run_module("flat_multi")
-
         data = self.pipeline.get_data("flat_cal")
         assert np.allclose(data[0, 10, 10], 0.00018817459122508784, rtol=limit, atol=0.)
         assert np.allclose(np.mean(data), 3.136868889624257e-07, rtol=limit, atol=0.)
         assert data.shape == (10, 100, 100)
-
-        data_multi = self.pipeline.get_data("flat_cal_multi")
-        assert np.allclose(data, data_multi, rtol=limit, atol=0.)
-        assert data.shape == data_multi.shape
