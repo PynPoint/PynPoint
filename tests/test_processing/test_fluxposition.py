1927a9c9f1080b1e4a2e6e10a955f1df37820678
Tomas Stolker <tomas.stolker@phys.ethz.ch>
2019-06-13 15:25:53 +0200

    test case update

--- tests/test_processing/test_fluxposition.py
+++ tests/test_processing/test_fluxposition.py
@@ -377,7 +377,7 @@ class TestFluxPosition:
         single = self.pipeline.get_data('mcmc')
         single = single[:, 20:, :].reshape((-1, 3))
         assert np.allclose(np.median(single[:, 0]), 0.148, rtol=0., atol=0.01)
-        assert np.allclose(np.median(single[:, 1]), 0., rtol=0., atol=0.2)
+        assert np.allclose(np.median(single[:, 1]), 0., rtol=0., atol=0.5)
         assert np.allclose(np.median(single[:, 2]), 0., rtol=0., atol=0.1)
 
     def test_mcmc_sampling_poisson(self):
