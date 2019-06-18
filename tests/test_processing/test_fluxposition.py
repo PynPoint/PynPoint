000f32a9a521b80fa7a9595e4306172c803fe3fd
Tomas Stolker <tomas.stolker@phys.ethz.ch>
2019-06-18 16:34:25 +0200

    test case fix

--- tests/test_processing/test_fluxposition.py
+++ tests/test_processing/test_fluxposition.py
@@ -343,8 +343,7 @@ class TestFluxPosition:
                                              'in an error or a different result.'
 
         data = self.pipeline.get_data('mcmc')
-        data = data[:, 20:, :].reshape((-1, 3))
-        print(np.median(data[:, 0]), np.median(data[:, 1]), np.median(data[:, 2]))
-        assert np.allclose(np.median(data[:, 0]), 0.15, rtol=0., atol=0.05)
-        assert np.allclose(np.median(data[:, 1]), 0., rtol=0., atol=0.5)
+        data = data[:, 50:, :].reshape((-1, 3))
+        assert np.allclose(np.median(data[:, 0]), 0.15, rtol=0., atol=0.07)
+        assert np.allclose(np.median(data[:, 1]), 0., rtol=0., atol=1.0)
         assert np.allclose(np.median(data[:, 2]), 0.5, rtol=0., atol=1.0)
