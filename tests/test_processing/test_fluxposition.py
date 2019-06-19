78383872bf7ae2cd7d34b0c8cd60c36b76e66970
Tomas Stolker <tomas.stolker@phys.ethz.ch>
2019-06-19 11:18:07 +0200

    unit test

--- tests/test_processing/test_fluxposition.py
+++ tests/test_processing/test_fluxposition.py
@@ -345,6 +345,6 @@ class TestFluxPosition:
 
         data = self.pipeline.get_data('mcmc')
         data = data[:, 50:, :].reshape((-1, 3))
-        assert np.allclose(np.median(data[:, 0]), 0.15, rtol=0., atol=0.07)
+        assert np.allclose(np.median(data[:, 0]), 0.15, rtol=0., atol=0.1)
         assert np.allclose(np.median(data[:, 1]), 0., rtol=0., atol=1.0)
-        assert np.allclose(np.median(data[:, 2]), 0.5, rtol=0., atol=1.0)
+        assert np.allclose(np.median(data[:, 2]), 0.0, rtol=0., atol=1.)
