7c5c47c46b4066e6e3a0d17c05fb62f9211d0de5
Anna Boehle <anna.boehle@gmail.com>
2019-07-09 15:22:02 +0200

    Added profile

--- pynpoint/util/psf.py
+++ pynpoint/util/psf.py
@@ -10,7 +10,7 @@ from scipy.ndimage import rotate
 from sklearn.decomposition import PCA
 
 
-#@profile
+@profile
 def pca_psf_subtraction(images: np.ndarray,
                         angles: np.ndarray,
                         pca_number: int,
