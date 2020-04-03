1c5b06161c1691059be4385b039decb47ee66cf8
Timothy Gebhard <mail@timothygebhard.de>
2020-04-03 16:57:43 +0200

    Update documentation

--- pynpoint/util/analysis.py
+++ pynpoint/util/analysis.py
@@ -49,8 +49,9 @@ def false_alarm(image: np.ndarray,
         The planet position (pix) along the vertical axis. The pixel coordinates of the bottom-left
         corner of the image are (-0.5, -0.5).
     size : float
-        The radius of the references apertures (in pixels). Usually, this values should be chosen
-        close to lambda over D, that is, the typical FWHM of the PSF.
+        The radius of the references apertures (in pixels). Usually, this values is chosen close to
+        one half of the typical FWHM of the PSF (0.514 lambda over D for a perfect Airy pattern; in
+        practice, however, the FWHM is often smaller than this).
     ignore : bool
         Whether or not to ignore the immediate neighboring apertures for the noise estimate. This is
         desirable in case there are "self-subtraction wings" left and right of the planet which
